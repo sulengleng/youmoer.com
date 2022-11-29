0bcc069ddf3d302d35f3d6c155b5e12c9557983d
yeyouchuan <yeyouchuan@onmail.com>
2022-11-29 23:17:48 +0000

    fixed

--- packages/web/src/queue.ts
+++
@@ -1,8 +0,0 @@
-export const initQueue = (): void => {
-  // initialise va until script is loaded
-  if (window.va) return;
-
-  window.va = function a(...params): void {
-    (window.vaq = window.vaq || []).push(params);
-  };
-};
