45e3e7718e3fc9d8287160bc6b4de8dc9a6c9427
yeyouchuan <yeyouchuan@onmail.com>
2022-11-30 14:55:10 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -115,7 +115,7 @@ export default function Home() {
           <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
             <div class="md:flex">
               <div class="md:shrink-0">
-                <img class="h-48 w-full object-cover md:h-full md:w-48" src="/img/building.jpg" alt="Modern building architecture"></img>
+                <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://files.catbox.moe/whnmix.png" alt="Modern building architecture"></img>
             </div>
               <div class="p-8">
                 <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">S.A.阿列克谢耶维奇</div>
