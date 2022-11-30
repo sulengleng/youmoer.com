be3f407a4acf39b13c606521dc92b0b4f0f6b431
yeyouchuan <yeyouchuan@onmail.com>
2022-11-30 14:27:52 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -113,11 +113,11 @@ export default function Home() {
           </h2>
           <br></br>
           <div class="quote--container">
-            <p className="italic text-left text-base quote">
+            <p className="italic text-left text-base">
               每个人都要做好自己的事。比如我自己的事就是写作，那就要写，要写下去，要找到一些答案，要帮助那些今天孤独的人，许多人都很迷茫，知识分子很迷茫，普通人就更迷茫了。所以，应该做好自己的事。
             </p>
             <br></br>
-            <p className="quote--author">
+            <p className="italic">
              —— S.A.阿列克谢耶维奇
             </p>
           </div>
