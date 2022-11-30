851a92dab58f2dd276dc9650746e7643f8c25ac3
yeyouchuan <yeyouchuan@onmail.com>
2022-11-30 19:25:52 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -117,7 +117,7 @@ export default function Home() {
 
         <br></br>
 
-        <div class="max-w-md mx-auto flex justify-center bg-[#821c20] rounded-xl shadow-md overflow-hidden sm:w-3/4">
+        <div class="max-w-md mx-auto flex justify-center bg-[#821c20] rounded-xl shadow-md overflow-hidden sm:w-1/2">
             <div class="md:flex">
               <div class="md:shrink-0">
                 <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://files.catbox.moe/whnmix.png" alt="Modern building architecture"></img>
