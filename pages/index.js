8019673410b68aededed6c0ae73c0299723170be
yeyouchuan <yeyouchuan@onmail.com>
2022-11-30 14:00:44 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -112,13 +112,15 @@ export default function Home() {
             封面短语
           </h2>
           <br></br>
-          <p className="text-left text-base quote">
-            每个人都要做好自己的事。比如我自己的事就是写作，那就要写，要写下去，要找到一些答案，要帮助那些今天孤独的人，许多人都很迷茫，知识分子很迷茫，普通人就更迷茫了。所以，应该做好自己的事。
-          </p>
-          <br></br>
-          <p className="quote--author">
-            —— S.A.阿列克谢耶维奇
-          </p>
+          <div class="quote--container">
+            <p className="text-left text-base quote">
+              每个人都要做好自己的事。比如我自己的事就是写作，那就要写，要写下去，要找到一些答案，要帮助那些今天孤独的人，许多人都很迷茫，知识分子很迷茫，普通人就更迷茫了。所以，应该做好自己的事。
+            </p>
+            <br></br>
+            <p className="quote--author">
+             —— S.A.阿列克谢耶维奇
+            </p>
+          </div>
         </ContentWrapper>
 
         <ContentWrapper width="520px" className="mt-10 dark:border-gray-800">
