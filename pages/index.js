40e6df46130e45315df3976f39bbac43d6ef6729
yeyouchuan <yeyouchuan@onmail.com>
2022-11-30 17:20:28 +0000

    color

--- pages/index.js
+++ pages/index.js
@@ -123,7 +123,7 @@ export default function Home() {
                 <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://files.catbox.moe/whnmix.png" alt="Modern building architecture"></img>
             </div>
               <div class="p-8">
-                <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">S.A.阿列克谢耶维奇</div>
+                <div class="uppercase tracking-wide text-sm text-[#166b41] font-semibold">S.A.阿列克谢耶维奇</div>
               < p class="mt-2 text-white">每个人都要做好自己的事。比如我自己的事就是<a class="underline decoration-indigo-500">写作</a>，那就要写，要写下去，要找到一些答案，要帮助那些今天孤独的人，许多人都很迷茫，知识分子很迷茫，普通人就更迷茫了。所以，<a class="underline decoration-pink-500">应该做好自己的事</a>。</p>
               </div>
             </div>
