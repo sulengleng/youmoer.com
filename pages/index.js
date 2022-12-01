9810b4e41f8a6d261b0de7ef328c75db8c4b5e93
yeyouchuan <yeyouchuan@onmail.com>
2022-12-01 11:57:21 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -142,13 +142,6 @@ export default function Home() {
           </div>
         </div>
 
-        <div class="snap-y snap-mandatory overflow-scroll">
-          <div class="snap-start bg-amber-200 w-screen h-screen flex items-center justify-center text-8xl">1</div>
-          <div class="snap-start bg-teal-200 w-screen  h-screen flex items-center justify-center text-8xl">2</div>
-          <div class="snap-start bg-cyan-200 w-screen h-screen flex items-center justify-center text-8xl">3</div>
-          <div class="snap-start bg-fuchsia-200 w-screen h-screen flex items-center justify-center text-8xl">4</div>
-        </div>
-
         <br></br>
         <br></br>
 
