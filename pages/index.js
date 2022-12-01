84495ff1e13c87e0dbb52e7781cbae8e5ce457b3
yeyouchuan <yeyouchuan@onmail.com>
2022-12-01 11:20:15 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -117,8 +117,19 @@ export default function Home() {
 
         <br></br>
 
-        <div class="flex flex-nowrap">
-          <div class="max-w-md mx-auto flex justify-center bg-[#821c20] rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="snap-mandatory snap-x">
+          <div class="max-w-md mx-auto flex justify-center bg-[#821c20] rounded-xl shadow-md overflow-hidden md:max-w-2xl snap-center">
+            <div class="md:flex">
+              <div class="md:shrink-0">
+                <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://files.catbox.moe/whnmix.png" alt="Modern building architecture"></img>
+            </div>
+              <div class="p-8">
+                <div class="uppercase tracking-wide text-sm text-[#166b41] font-semibold">S.A.阿列克谢耶维奇</div>
+              < p class="mt-2 text-white">每个人都要做好自己的事。比如我自己的事就是写作，那就要写，要写下去，要找到一些答案，要帮助那些今天孤独的人，许多人都很迷茫，知识分子很迷茫，普通人就更迷茫了。所以，<a class="underline decoration-pink-500">应该做好自己的事</a>。</p>
+              </div>
+            </div>
+          </div>
+          <div class="max-w-md mx-auto flex justify-center bg-[#821c20] rounded-xl shadow-md overflow-hidden md:max-w-2xl snap-center">
             <div class="md:flex">
               <div class="md:shrink-0">
                 <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://files.catbox.moe/whnmix.png" alt="Modern building architecture"></img>
