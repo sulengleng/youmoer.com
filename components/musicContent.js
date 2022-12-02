3b9c86c76ec0801315ee8678c7e20411f92a8494
yeyouchuan <yeyouchuan@onmail.com>
2022-12-02 15:41:46 +0000

    test

--- components/musicContent.js
+++ components/musicContent.js
@@ -65,8 +65,6 @@ export default function MusicContent({ post }) {
         ) : (
           ""
         )}
-      </div>
-      <div className="flex justify-center items-center space-x-5 max-w-[620px] w-full mt-5 mb-10">
         {post?.site ? (
           <iframe 
             allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" 
