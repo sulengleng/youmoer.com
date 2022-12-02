3310d7b792b4f5c2121c454e1c5527944e43c257
yeyouchuan <yeyouchuan@onmail.com>
2022-12-02 15:34:37 +0000

    update

--- components/musicContent.js
+++ components/musicContent.js
@@ -67,14 +67,18 @@ export default function MusicContent({ post }) {
         )}
       </div>
       <div>
-        <iframe 
-          allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" 
-          frameborder="0" 
-          height="450" 
-          style="width:100%;max-width:660px;overflow:hidden;background:transparent;" 
-          sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
-          src="post?.site">
-        </iframe>
+        {post?.site ? (
+          <iframe 
+            allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write" 
+            frameborder="0" 
+            height="450" 
+            style="width:100%;max-width:660px;overflow:hidden;background:transparent;" 
+            sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
+            src="post?.site">
+          </iframe>
+        ) : (
+          ""
+        )}
       </div>
       <div
         dangerouslySetInnerHTML={{ __html: post?.content }}
