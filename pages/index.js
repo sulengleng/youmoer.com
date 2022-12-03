b2151fdd39797900fd88235e6a6804dfe9b2402d
yeyouchuan <yeyouchuan@onmail.com>
2022-12-03 18:34:26 +0000

    delete md 2*1

--- pages/index.js
+++ pages/index.js
@@ -232,7 +232,7 @@ export default function Home() {
 
       <br></br>
 
-      <fieldset className="flex rounded-xl shadow-lg overflow-hidden p-4 bg-[#171717]">
+      <fieldset className="mx-auto flex rounded-xl shadow-lg overflow-hidden p-4 bg-[#171717]">
         <h3>Published status</h3>
 
         <br></br>
