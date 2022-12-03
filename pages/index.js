2e61ad56f420f2883dac7a2dcd3338cd7c2256b5
yeyouchuan <yeyouchuan@onmail.com>
2022-12-03 17:49:06 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -229,6 +229,22 @@ export default function Home() {
           </div>
         </ContentWrapper>
       </motion.div>
+
+      <br></br>
+
+      <fieldset>
+        <legend>Published status</legend>
+
+        <input id="draft" class="peer/draft" type="radio" name="status" checked />
+        <label for="draft" class="peer-checked/draft:text-sky-500">Draft</label>
+
+        <input id="published" class="peer/published" type="radio" name="status" />
+        <label for="published" class="peer-checked/published:text-sky-500">Published</label>
+
+        <div class="hidden peer-checked/draft:block">Drafts are only visible to administrators.</div>
+        <div class="hidden peer-checked/published:block">Your post will be publicly visible on your site.</div>
+      </fieldset>
+      
     </div>
   );
 }
