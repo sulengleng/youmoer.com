9ca60381d059164c443dcdb5e5762046ff59152d
yeyouchuan <yeyouchuan@onmail.com>
2022-12-03 16:15:58 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -94,7 +94,7 @@ export default function Home() {
                 </p>
                 <br />
                 <p>
-                  最后要感谢<link href="https://github.com/rishimohan" target="_blank">@rishimohan</link>给我提供的模板和一些细节上的帮助
+                  最后要感谢<Link href="https://github.com/rishimohan" target="_blank">@rishimohan</Link>给我提供的模板和一些细节上的帮助
                 </p>
               </ul>
             </div>
