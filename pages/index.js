de2a652d63c53a5ef6403178ea913d9c05e20ff0
yeyouchuan <yeyouchuan@onmail.com>
2022-12-03 18:14:15 +0000

    color

--- pages/index.js
+++ pages/index.js
@@ -232,7 +232,7 @@ export default function Home() {
 
       <br></br>
 
-      <fieldset className="mx-auto md:max-w-2xl flex rounded-xl shadow-lg overflow-hidden p-4">
+      <fieldset className="mx-auto md:max-w-2xl flex rounded-xl shadow-lg overflow-hidden p-4 bg-slate-600">
         <legend>Published status</legend>
 
         <input id="draft" class="peer/draft" type="radio" name="status" checked />
