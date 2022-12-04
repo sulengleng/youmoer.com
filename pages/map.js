9478dfb0d9df4c0c4b63912bb6b23cbf1aaaf788
yeyouchuan <yeyouchuan@onmail.com>
2022-12-04 00:05:47 +0000

    fix size

--- pages/map.js
+++ pages/map.js
@@ -37,13 +37,15 @@ export default function Home() {
             欢迎来到我的旅行车，我创建了这个页面来记录我去过的地方。
           </p>
           <div>
-            你可以点击地图上的点来查看我去过的地方。也可以点击 <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
-  <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-      Green to blue
-  </span>
-</button> 按钮来获得更多的信息。
+            你可以点击地图上的点来查看我去过的地方。也可以点击 
+            <button 
+              class="relative inline-flex items-center justify-center p-0.5 mb-0.5 mr-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+              <span class="relative px-2.5 py-1 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Green to blue
+              </span>
+            </button> 
+            按钮来获得更多的信息。
           </div>
-          <button class="rounded-full text-green-800 after:content-['>'] font-bold">Open Map</button>
         </div>
       </article>
     </>
