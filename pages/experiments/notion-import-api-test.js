1d19f5d542db80021a98640f15d1648a3bfd920c
yeyouchuan <yeyouchuan@onmail.com>
2022-12-06 00:30:07 +0000

    update

--- pages/experiments/notion-import-api-test.js
+++ pages/experiments/notion-import-api-test.js
@@ -1,29 +1,50 @@
 const { Client } = require("@notionhq/client")
 
-const notion = new Client({
-    auth: "secret_t2IvXIXBYP9O4fAJ0wHCRChCmW3ADR11Chn9eu99Et7",
-})
+export default function NotionImportApiTest() {
+  <ul className={list()}>
+  {list?.map((item) => (
+    <StackTile
+      key={item.id}
+      title={item.properties.Name.title[0].plain_text}
+      url={item.properties.URL.url}
+      tags={item.properties.Tags.multi_select}
+    />
+  ))}
+</ul>
+}
 
-;(async() => {
-    const response = await notion.databases.query({
-        database_id: "9b5e911262dd4899b8857992f19b3d99",
-        filter: {
-            and: [
-              {
-                property: "Show",
-                checkbox: {
-                  equals: true,
+export async function getStaticProps() {
+  const notion = new Client({
+      auth: "secret_t2IvXIXBYP9O4fAJ0wHCRChCmW3ADR11Chn9eu99Et7",
+  })
+
+  ;(async() => {
+      const response = await notion.databases.query({
+          database_id: "9b5e911262dd4899b8857992f19b3d99",
+          filter: {
+              and: [
+                {
+                  property: "Show",
+                  checkbox: {
+                    equals: true,
+                  },
                 },
+              ],
+            },
+            sorts: [
+              {
+                property: "Name",
+                direction: "ascending",
               },
             ],
-          },
-          sorts: [
-            {
-              property: "Name",
-              direction: "ascending",
-            },
-          ],
-        })
+          })
 
-    console.log(response.results)
-})()
+          return {
+            props: {
+              list: response.results,
+            },
+            revalidate: 60,
+          }
+        }
+      )()
+}
