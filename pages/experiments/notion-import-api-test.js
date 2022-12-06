bbd5f0574e74418ce111b6c0ce398b574f23f613
yeyouchuan <yeyouchuan@onmail.com>
2022-12-06 00:37:25 +0000

    update

--- pages/experiments/notion-import-api-test.js
+++ pages/experiments/notion-import-api-test.js
@@ -18,33 +18,31 @@ export async function getStaticProps() {
       auth: "secret_t2IvXIXBYP9O4fAJ0wHCRChCmW3ADR11Chn9eu99Et7",
   })
 
-  ;(async() => {
-      const response = await notion.databases.query({
-          database_id: "9b5e911262dd4899b8857992f19b3d99",
-          filter: {
-              and: [
-                {
-                  property: "Show",
-                  checkbox: {
-                    equals: true,
-                  },
-                },
-              ],
-            },
-            sorts: [
-              {
-                property: "Name",
-                direction: "ascending",
+  const response = await notion.databases.query({
+      database_id: "9b5e911262dd4899b8857992f19b3d99",
+      filter: {
+          and: [
+            {
+              property: "Show",
+              checkbox: {
+                equals: true,
               },
-            ],
-          })
-
-          return {
-            props: {
-              list: response.results,
             },
-            revalidate: 60,
-          }
-        }
-      )()
-}
+          ],
+        },
+        sorts: [
+          {
+            property: "Name",
+            direction: "ascending",
+          },
+        ],
+      })
+
+      return {
+        props: {
+          list: response.results,
+        },
+        revalidate: 60,
+      }
+    }
+    
