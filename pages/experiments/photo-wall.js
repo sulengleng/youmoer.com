1451e8bfa07c03f8ed9364ecd17842dfde03840d
yeyouchuan <yeyouchuan@onmail.com>
2022-12-06 15:05:53 +0000

    update

--- pages/experiments/photo-wall.js
+++ pages/experiments/photo-wall.js
@@ -38,36 +38,40 @@ const IMAGES = [
     </div>
   );
 
-<div className="pb-20">
-     <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-            transition: { duration: 0.35, delay: 0.3 },
-          }}
-        >
-        <ContentWrapper className="gap-8 columns-3">
-            {IMAGES?.map((item) => (
-            <div
-                key={item.src}
-                className={clsx(
-                item?.className,
-                "relative border p-[4px] shadow-[0_0_32px_rgba(0,0,0,0.1)] rounded-[12px] dark:border-gray-700"
-                )}
+export default function Photo() {
+  return (
+    <div className="pb-20">
+        <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{
+                opacity: 1,
+                y: 0,
+                transition: { duration: 0.35, delay: 0.3 },
+            }}
             >
-                <img
-                src={item?.src}
-                alt={item?.place}
-                className="rounded-[9px]"
-                loading="lazy"
-                />
-                <div className="font-mono text-xs absolute bottom-[4%] left-[4%] inline-flex bg-white/90 backdrop-blur rounded-[10px] px-2 py-px items-center dark:text-black">
-                <MapPin size={14} className="mr-1" />
-                {item?.place}
+            <ContentWrapper className="gap-8 columns-3">
+                {IMAGES?.map((item) => (
+                <div
+                    key={item.src}
+                    className={clsx(
+                    item?.className,
+                    "relative border p-[4px] shadow-[0_0_32px_rgba(0,0,0,0.1)] rounded-[12px] dark:border-gray-700"
+                    )}
+                >
+                    <img
+                    src={item?.src}
+                    alt={item?.place}
+                    className="rounded-[9px]"
+                    loading="lazy"
+                    />
+                    <div className="font-mono text-xs absolute bottom-[4%] left-[4%] inline-flex bg-white/90 backdrop-blur rounded-[10px] px-2 py-px items-center dark:text-black">
+                    <MapPin size={14} className="mr-1" />
+                    {item?.place}
+                    </div>
                 </div>
-            </div>
-            ))}
-        </ContentWrapper>
-    </motion.div>
-</div>
+                ))}
+            </ContentWrapper>
+        </motion.div>
+    </div>
+    )
+}
