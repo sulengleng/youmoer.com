5c56ec04f997b9d6eab3a5dec410581094b5598b
yeyouchuan <yeyouchuan@onmail.com>
2022-12-06 15:47:58 +0000

    update

--- pages/experiments/photo-wall.js
+++ pages/experiments/photo-wall.js
@@ -5,14 +5,14 @@ import { motion } from 'framer-motion'
 
 const IMAGES = [
     {
-      src: "/images/pages/Statue-and-Dove.png",
-      place: "Bristol",
-      className: "rotate-[0deg]",
+        src: "https://freight.cargo.site/w/700/q/75/i/a0e3c4fc1324dbf9c3fdf6b44e0f4a87b7e153b47f7cb5382caf6c40854e7e49/3R6A9398.jpg",
+        place: "Bristol",
+        className: "rotate-[0deg]",
     },
     {
-      src: "/images/pages/Street.png",
-      place: "York",
-      className: "rotate-[0deg]",
+        src: "https://freight.cargo.site/w/700/q/75/i/5b1f5279aede4a43d1b0eb34f1e145ac95feac92e1cd414ec8ba180c54e85feb/88.jpg",
+        place: "York",
+        className: "rotate-[0deg]",
     },
     {
         src: "https://files.catbox.moe/23b07j.png", 
@@ -20,14 +20,24 @@ const IMAGES = [
         className: "rotate-[0deg]", 
       },
     {
-      src: "/images/pages/dali.png",
-      place: "Dali",
-      className: "rotate-[0deg]",
+        src: "https://freight.cargo.site/w/700/q/75/i/f49dd3a81b6a0f2bfd5bb619c4a7e43bc4dc28b509c779a0e338f7ec2babbae7/cascading_pigments_2.jpg",
+        place: "Dali",
+        className: "rotate-[0deg]",
     },
     {
-      src: "/images/pages/pine-and-stone.png",
-      place: "Suzhou",
-      className: "rotate-[0deg]",
+        src: "https://freight.cargo.site/w/700/q/75/i/0c7f100a02d58442dbb01017a9525a60bc9d403fe20ca0814c3836cdcc4f80d8/terraces.jpg",
+        place: "Suzhou",
+        className: "rotate-[0deg]",
+    },
+    {
+        src: "https://freight.cargo.site/w/700/q/75/i/0a59d1d90e57cc54613ac65c0e09f72beba7db92a0cbb42a7f24b7e4ee0faaf8/US_old_9.jpg",
+        note: "This is a test",
+        className: "rotate-[0deg]",
+    },
+    {
+        src: "https://freight.cargo.site/w/700/q/75/i/15b75ddcb51f4749fd5d9f7e44fda6175dee3d66a491272b35b89060190a107d/Idriveatnight.jpg",
+        note: "This is a test",
+        className: "rotate-[0deg]",
     }
   ];
   
