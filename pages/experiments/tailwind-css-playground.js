40e536de0eae831c19cecbd4c932d02030ff4b0d
yeyouchuan <yeyouchuan@onmail.com>
2022-12-07 02:02:39 +0000

    update

--- pages/experiments/tailwind-css-playground.js
+++ pages/experiments/tailwind-css-playground.js
@@ -49,7 +49,7 @@ return (
                                 <a href="#" class="flex items-center font-medium text-blue-600 dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700">Read more <svg class="w-4 h-4 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg></a>
                             </div>
                         </div>
-                        <img src="/docs/images/popovers/italy.png" class="h-full col-span-2" alt="Italy map" />
+                        <img src="https://files.catbox.moe/0q7wri.png" class="h-full col-span-2" alt="Italy map" />
                     </div>
                     <div data-popper-arrow></div>
                 </div>
