8d703db04a392170aa7a744f93a41e52dcd91198
yeyouchuan <yeyouchuan@onmail.com>
2022-12-07 17:57:41 +0000

    fixed

--- pages/experiments/tailwind-css-playground.js
+++ pages/experiments/tailwind-css-playground.js
@@ -37,23 +37,6 @@ return (
                     <h1 class="p-8 max-w-2xl text-8xl font-black uppercase text-white transform -rotate-6 mix-blend-overlay">Tailwind css is super cool!</h1>
                 </div>
             </div>  
-            <div>
-                <script src="../path/to/flowbite/dist/flowbite.js"></script>
-                <p class="font-light text-gray-500 dark:text-gray-400">Due to its central geographic location in Southern Europe, <a href="#" class="text-blue-600 underline dark:text-blue-500 hover:no-underline" data-popover-target="popover-image">Italy</a> has historically been home to myriad peoples and cultures. In addition to the various ancient peoples dispersed throughout what is now modern-day Italy, the most predominant being the Indo-European Italic peoples who gave the peninsula its name, beginning from the classical era, Phoenicians and Carthaginians founded colonies mostly in insular Italy</p>
-                <div data-popover id="popover-image" role="tooltip" class="absolute z-10 invisible inline-block text-sm font-light text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-96 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600">
-                    <div class="grid grid-cols-5">
-                        <div class="col-span-3 p-3">
-                            <div class="space-y-2">
-                                <h3 class="font-semibold text-gray-900 dark:text-white">About Italy</h3>
-                                <p>Italy is located in the middle of the Mediterranean Sea, in Southern Europe it is also considered part of Western Europe. A unitary parliamentary republic with Rome as its capital and largest city.</p>
-                                <a href="#" class="flex items-center font-medium text-blue-600 dark:text-blue-500 dark:hover:text-blue-600 hover:text-blue-700">Read more <svg class="w-4 h-4 ml-1" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg></a>
-                            </div>
-                        </div>
-                        <img src="https://files.catbox.moe/0q7wri.png" class="h-full col-span-2" alt="Italy map" />
-                    </div>
-                    <div data-popper-arrow></div>
-                </div>
-            </div> 
         </div>
     </body>
 );
