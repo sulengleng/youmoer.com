82f8780071ec04d0cf690f14a418b60ae4d6cbd5
yeyouchuan <yeyouchuan@onmail.com>
2022-12-09 20:38:56 +0000

    update index

--- pages/index.js
+++ pages/index.js
@@ -130,20 +130,29 @@ export default function Home() {
           </ContentWrapper>
 
           <br></br>
-
-          <div class="snap-mandatory snap-x">
-            <div class="max-w-[80%] mx-auto md:max-w-[70%] flex justify-center bg-[#821c20] rounded-xl shadow-lg overflow-hidden snap-center lg:max-w-[60%]">
-              <div class="md:flex">
-                <div class="md:shrink-0">
-                  <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://files.catbox.moe/whnmix.png" alt="Modern building architecture"></img>
-                </div>
-                <div class="p-8">
-                  <div class="uppercase tracking-wide text-sm text-[#166b41] dark:text-green-700 font-semibold">S.A.阿列克谢耶维奇</div>
-                  <p class="mt-2 text-white">每个人都要做好自己的事。比如我自己的事就是写作，那就要写，要写下去，要找到一些答案，要帮助那些今天孤独的人，许多人都很迷茫，知识分子很迷茫，普通人就更迷茫了。所以，<a class="underline decoration-pink-500">应该做好自己的事</a>。</p>
+          <motion.div 
+            whileHover={{ 
+                scale: 1.05,
+              }}
+            whileTap={{
+                scale: 0.95,
+            }}
+            drag
+            >
+            <div class="snap-mandatory snap-x">
+              <div class="max-w-[80%] mx-auto md:max-w-[70%] flex justify-center bg-[#821c20] rounded-xl shadow-lg overflow-hidden snap-center lg:max-w-[60%]">
+                <div class="md:flex">
+                  <div class="md:shrink-0">
+                    <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://files.catbox.moe/whnmix.png" alt="Modern building architecture"></img>
+                  </div>
+                  <div class="p-8">
+                    <div class="uppercase tracking-wide text-sm text-[#166b41] dark:text-green-700 font-semibold">S.A.阿列克谢耶维奇</div>
+                    <p class="mt-2 text-white">每个人都要做好自己的事。比如我自己的事就是写作，那就要写，要写下去，要找到一些答案，要帮助那些今天孤独的人，许多人都很迷茫，知识分子很迷茫，普通人就更迷茫了。所以，<a class="underline decoration-pink-500">应该做好自己的事</a>。</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </motion.div>
 
           <br></br>
           <br></br>
