76b92bcf6e0481e0b032d96f6146f11724e66429
yeyouchuan <yeyouchuan@onmail.com>
2022-12-15 19:39:56 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -2,6 +2,32 @@ import clsx from "clsx";
 import Link from "next/link";
 import Head from "next/head";
 import {NextSeo} from "next-seo";
+import {ArrowCircleRight, ArrowSquareOut, Star} from "phosphor-react";
+
+const TIMELINE = [
+    {
+        date: 'Nov 23, 2022',
+        title: '个人网站开始上线',
+        description: '也欢迎你来我之前的数字花园 https://yeyouchuan.top'
+    },
+    {
+        date: 'Sep 28, 2022',
+        title: '开始大学学习',
+        description: '约克大学 Bsc Interactive Media'
+    },
+]
+
+const ContentWrapper = ({ children, className, width }) => (
+    <div
+        style={{ maxWidth: `${width || '100%'}`}}
+        className={clsx(
+            "w-full px-5 mx-auto relative",
+            className
+        )}
+    >
+        {children}
+    </div>
+);
 
 export default function Home() {
   return (
@@ -24,6 +50,50 @@ export default function Home() {
 
       <article className="w-full px-5 pb-10 overflow-y-auto max-w-[620px] mx-auto">
         <h2 className="mb-6 text-4xl font-bold md:text-5xl pt-14">仿生人夜游船(2022批次)使用手册</h2>
+
+          <ContentWrapper width="700px" className="ml-auto mb-4 sm:text-left">
+              <h2 className="font-bold text-lg flex items-center">
+                  <Star size={20} className="mr-2 opacity-40" />
+                  2022年时间线
+              </h2>
+          </ContentWrapper>
+
+          <br></br>
+
+          <ContentWrapper
+              width="700px"
+              className="border dark:border-gray-800 pt-6 bg-gray-50 md:rounded-lg dark:bg-gray-900"
+          >
+              <div className="relative pl-8">
+                  <div className="h-full w-[1px] bg-gray-200 dark:bg-gray-800 left-[10px] top-[5px] absolute"></div>
+                  {TIMELINE?.map((item, index) => (
+                      <div key={item.title + index} className="mb-10 relative">
+                          <div className="w-3 h-3 rounded-full absolute left-[-30px] top-[16px]">
+                              <ArrowCircleRight
+                                  size={18}
+                                  className="bg-white text-gray-400 dark:text-gray-400 dark:bg-gray-900 rounded-full"
+                              />
+                          </div>
+                          <p className="opacity-40 text-xs">{item.date}</p>
+
+                          <h3 className="leading-tight mb-1 font-semibold text-sm md:text-base inline-block">
+                              <span className="">{item.title}</span>
+                              {item?.link ? (
+                                  <Link href={item.link} target="_blank">
+                        <span className="inline-block ml-1 opacity-80">
+                          <ArrowSquareOut size={12} />
+                        </span>
+                                  </Link>
+                              ) : (
+                                  ""
+                              )}
+                          </h3>
+                          <p className="opacity-60 text-sm">{item.description}</p>
+                      </div>
+                  ))}
+              </div>
+          </ContentWrapper>
+
         <div className="post-content">
             <a className="text-[#6868ac]">年度颜色</a>
         </div>
