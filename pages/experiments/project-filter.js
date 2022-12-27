5a9853c4a519e2bdde535e9ca99cba986143c057
yeyouchuan <yeyouchuan@onmail.com>
2022-12-27 15:56:38 +0000

    add forms

--- pages/experiments/project-filter.js
+++ pages/experiments/project-filter.js
@@ -10,46 +10,27 @@ const sortOptions = [
     { name: 'Price: High to Low', href: '#', current: false },
 ]
 const subCategories = [
-    { name: 'Totes', href: '#' },
-    { name: 'Backpacks', href: '#' },
-    { name: 'Travel Bags', href: '#' },
-    { name: 'Hip Bags', href: '#' },
-    { name: 'Laptop Sleeves', href: '#' },
+    { name: '綾井海荷（OC）', href: '#' },
+    { name: '澤木海生（OC）', href: '#' },
+    { name: '科搜研', href: '#' },
+    { name: '特摄', href: '#' },
+    { name: '其他同人', href: '#' },
 ]
 const filters = [
     {
-        id: 'color',
-        name: 'Color',
+        id: 'Work',
+        name: '一点打工实录',
         options: [
-            { value: 'white', label: 'White', checked: false },
-            { value: 'beige', label: 'Beige', checked: false },
-            { value: 'blue', label: 'Blue', checked: true },
-            { value: 'brown', label: 'Brown', checked: false },
-            { value: 'green', label: 'Green', checked: false },
-            { value: 'purple', label: 'Purple', checked: false },
+            { value: 'afk', label: 'AFK IP部平面设计', checked: false },
+            { value: 'light', label: '以闪亮之名 发行平面设计', checked: false },
         ],
     },
     {
-        id: 'category',
-        name: 'Category',
+        id: 'Practice',
+        name: '一些练习',
         options: [
-            { value: 'new-arrivals', label: 'New Arrivals', checked: false },
-            { value: 'sale', label: 'Sale', checked: false },
-            { value: 'travel', label: 'Travel', checked: true },
-            { value: 'organization', label: 'Organization', checked: false },
-            { value: 'accessories', label: 'Accessories', checked: false },
-        ],
-    },
-    {
-        id: 'size',
-        name: 'Size',
-        options: [
-            { value: '2l', label: '2L', checked: false },
-            { value: '6l', label: '6L', checked: false },
-            { value: '12l', label: '12L', checked: false },
-            { value: '18l', label: '18L', checked: false },
-            { value: '20l', label: '20L', checked: false },
-            { value: '40l', label: '40L', checked: true },
+            { value: 'redesign', label: '曲名Redesign', checked: false },
+            { value: '2022', label: '作字夏フェス2022', checked: false },
         ],
     },
 ]
