5fe2f22a1de47ab47f8f1256a13aed836d434262
yeyouchuan <yeyouchuan@onmail.com>
2022-12-27 18:28:33 +0000

    add forms

--- pages/experiments/project-filter.js
+++ pages/experiments/project-filter.js
@@ -35,21 +35,25 @@ const IMAGES = [
         src: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/2f9d219e-a589-4b5b-8769-375b53ad3427/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221227%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221227T180047Z&X-Amz-Expires=86400&X-Amz-Signature=f8b3e1100a021026c27bafe83d5e548428c8d144447b6029bcd83a3c899eab57&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Untitled.png%22&x-id=GetObject",
         note: "娑丽丝",
         className: "rotate-[0deg]",
+        label: "AFK IP部平面设计",
     },
     {
         src: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/ee2d7576-0c1a-48ed-a2a5-e6e7e14b1d8b/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221227%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221227T182219Z&X-Amz-Expires=86400&X-Amz-Signature=b45d5adc3f3f6f927cd8bbb8937405c6a2092a4df62ed09e0ce62f4362a64d69&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Untitled.png%22&x-id=GetObject",
         note: "艾伦",
         className: "rotate-[0deg]",
+        label: "AFK IP部平面设计",
     },
     {
         src: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/a51668d3-dbbb-434d-8a39-6b0729ffb7f6/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221227%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221227T182337Z&X-Amz-Expires=86400&X-Amz-Signature=da95a7337e6c14df7899e5ca9978c20a7d3003105ad5feeef9bbc39c0945fe46&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Untitled.png%22&x-id=GetObject",
         note: "猎魔人",
         className: "rotate-[0deg]",
+        label: "AFK IP部平面设计",
     },
     {
         src: "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/3767f49b-afcd-4cae-926b-3ca525611f35/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221227%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221227T182331Z&X-Amz-Expires=86400&X-Amz-Signature=171db3491914208a04cf8a3269856ef9b065e3c6b1b44167074520b128719e43&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Untitled.png%22&x-id=GetObject",
         note: "塔尼",
         className: "rotate-[0deg]",
+        label: "AFK IP部平面设计",
     }
 ]
 
