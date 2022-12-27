00c6d9c212e08a5723586b09cdbff544da111992
yeyouchuan <yeyouchuan@onmail.com>
2022-12-27 20:12:04 +0000

    test photo

--- pages/experiments/photo-wall.js
+++ pages/experiments/photo-wall.js
@@ -131,7 +131,7 @@ export default function Photo() {
                     transition: { duration: 0.35, delay: 0.3 },
                 }}
             >
-                <ContentWrapper className="gap-6 columns-3 space-y-6 pt-6 md:columns-auto">
+                <ContentWrapper className="gap-6 columns-3 space-y-6 pt-6 md:columns-1">
                     {IMAGES?.map((item) => (
                     <div
                         key={item.src}
