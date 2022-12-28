21997377edc8ab151546432ff391a2dccc83e218
yeyouchuan <yeyouchuan@onmail.com>
2022-12-28 19:57:09 +0000

    update filter

--- pages/experiments/project-filter.js
+++ pages/experiments/project-filter.js
@@ -137,7 +137,7 @@ export default function Example() {
                                                                             defaultValue={option.value}
                                                                             type="checkbox"
                                                                             defaultChecked={option.checked}
-                                                                            checked={selectedLabel.includes(options)}
+                                                                            checked={selectedLabel.includes(labelTitle)}
                                                                             onChange={(e) => onchangeLabel(label, e.target.checked)}
                                                                             className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                                                                         />
@@ -234,7 +234,7 @@ export default function Example() {
                                                                     defaultValue={option.value}
                                                                     type="checkbox"
                                                                     defaultChecked={option.checked}
-                                                                    checked={selectedLabel.includes(options)}
+                                                                    checked={selectedLabel.includes(labelTitle)}
                                                                     onChange={(e) => onchangeLabel(label, e.target.checked)}
                                                                     className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                                                                 />
