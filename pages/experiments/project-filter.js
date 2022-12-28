8f48e2630ce070d956317da032106f97b045fc0f
yeyouchuan <yeyouchuan@onmail.com>
2022-12-28 13:46:44 +0000

    update filter

--- pages/experiments/project-filter.js
+++ pages/experiments/project-filter.js
@@ -222,7 +222,7 @@ export default function Example() {
                             </form>
 
                             {/* Product grid */}
-                            <div className="col-span-2">
+                            <div className="col-start-2 col-span-2">
                                 {/* Replace with your content */}
                                 {products.map((product) => (
                                     <ProductPhoto product={product} />
