b9428196e3ec85eb714930bdef3f7e31a6b0434b
yeyouchuan <yeyouchuan@onmail.com>
2022-12-29 12:11:22 +0000

    update filter

--- pages/experiments/project-filter.jsx
+++ pages/experiments/project-filter.jsx
@@ -21,7 +21,7 @@ const ContentWrapper = ({ children, className, width }) => (
 
 export default function Project() {
     let [categories] = useState({
-        Recent: [
+        AFK: [
             {
                 src: "https://s2.loli.net/2022/12/28/sWV5Pf74vtypSzN.png",
                 note: "娑丽丝",
@@ -29,7 +29,7 @@ export default function Project() {
                 className: "rotate-[0deg]",
             },
         ],
-        Popular: [
+        曲名Redesign: [
             {
                 src: "https://s2.loli.net/2022/12/28/YWM7rcb3NH6AZgh.png",
                 note: "夏疾风",
@@ -37,11 +37,19 @@ export default function Project() {
                 className: "rotate-[0deg]",
             },
         ],
-        Trending: [
+        以闪亮之名: [
             {
                 src: "https://s2.loli.net/2022/12/28/BQ7s5l9Joi24tdx.png",
                 note: "Have a Date with Romance",
                 label: "以闪亮之名 发行平面设计",
+                className: "rotate-[0deg]",
+            },
+        ],
+        作字夏フェス2022: [
+            {
+                src: "https://s2.loli.net/2022/12/28/RDwFAjn8yKd3lpJ.png",
+                note: "灯火",
+                label: "作字夏フェス2022",
                 className: "rotate-[0deg]",
             },
         ],
