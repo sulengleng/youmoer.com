fa20e46ecc80363f312b6006fbe291378d195a9b
yeyouchuan <yeyouchuan@onmail.com>
2022-12-30 17:41:11 +0000

    add music player

--- components/musicPlayer.js
+++ components/musicPlayer.js
@@ -13,11 +13,11 @@ export default function Player() {
             <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
                 <div className="md:flex">
                     <div className="md:shrink-0">
-                        <div className="absolute place-items-center h-36 w-full hover:bg-gray-500/80">
+                        <div className="absolute h-36 w-36 hover:bg-gray-500/80">
                             {isPlaying ? (
-                                <PauseCircle size={76} className="opacity-50"  color="#f6f5f6" onClick={togglePlay} />
+                                <PauseCircle size={76} className="opacity-50 place-items-center"  color="#f6f5f6" onClick={togglePlay} />
                             ) : (
-                                <PlayCircle size={76} className="opacity-50"  color="#f6f5f6" onClick={togglePlay} />
+                                <PlayCircle size={76} className="opacity-50 place-items-center"  color="#f6f5f6" onClick={togglePlay} />
                             )}
                         </div>
                         <img className="h-36 w-full object-cover md:h-full md:w-36" src="https://files.catbox.moe/9i0d2g.png"
