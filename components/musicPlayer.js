2465557168a1e64e7a16b9260ad8a3daecfefda7
yeyouchuan <yeyouchuan@onmail.com>
2022-12-30 19:14:05 +0000

    add music player

--- components/musicPlayer.js
+++ components/musicPlayer.js
@@ -20,7 +20,7 @@ export default function Player() {
                                 <PlayCircle size={48} className="opacity-50"  color="#f6f5f6" onClick={togglePlay} />
                             )}
                         </div>
-                        <img className="h-20 w-full md:place-items-center object-cover md:h-[60%] md:w-full rounded-md ml-4" src="https://files.catbox.moe/9i0d2g.png"
+                        <img className="h-20 w-full md:place-items-center object-cover md:h-[60%] md:w-auto rounded-md ml-4" src="https://files.catbox.moe/9i0d2g.png"
                              alt="Music cover" />
                     </div>
                     <div className="p-8">
