f3cedc2f39709b07dd1b8bcfddf3d627a3c2d918
yeyouchuan <yeyouchuan@onmail.com>
2022-12-30 18:40:41 +0000

    add music player

--- components/musicPlayer.js
+++ components/musicPlayer.js
@@ -12,22 +12,21 @@ export default function Player() {
         <div className="player pt-6">
             <div className="max-w-md mx-auto bg-gradient-to-r from-black via-[#041c0c] to-[#062a13] rounded-xl shadow-md overflow-hidden md:max-w-2xl">
                 <div className="md:flex">
-                    <div className="md:shrink-0 h-20 w-20 place-self-center">
-                        <div className="absolute h-20 w-20 place-items-center">
+                    <div className="md:shrink-0 h-48 w-48 place-self-center">
+                        <div className="absolute h-48 w-48 place-items-center">
                             {isPlaying ? (
-                                <PauseCircle size={76} className="opacity-50 place-self-center"  color="#f6f5f6" onClick={togglePlay} />
+                                <PauseCircle size={48} className="opacity-50 place-self-center"  color="#f6f5f6" onClick={togglePlay} />
                             ) : (
-                                <PlayCircle size={76} className="opacity-50 place-self-center"  color="#f6f5f6" onClick={togglePlay} />
+                                <PlayCircle size={48} className="opacity-50 place-self-center"  color="#f6f5f6" onClick={togglePlay} />
                             )}
                         </div>
-                        <img className="h-20 w-full object-cover md:h-full md:w-20 rounded-md pl-4" src="https://files.catbox.moe/9i0d2g.png"
+                        <img className="h-20 w-full object-cover md:h-full md:aspect-square rounded-md pl-4" src="https://files.catbox.moe/9i0d2g.png"
                              alt="Music cover" />
                     </div>
                     <div className="p-8">
-                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats
+                        <div className="uppercase tracking-wide text-sm text-gray-500 font-medium">万能青年旅店
                         </div>
-                        <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible
-                            accomodation for your team</a>
+                        <a href="#" className="block mt-1 text-lg leading-tight font-semibold text-white hover:underline">十万嬉皮</a>
                     </div>
                 </div>
             </div>
