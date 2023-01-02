f14adc8dd5a9fbef65fb9747871f89d701fc05ca
yeyouchuan <yeyouchuan@onmail.com>
2023-01-02 15:43:05 +0000

    update music_player.tsx

--- pages/experiments/music-player.js
+++ pages/experiments/music-player.js
@@ -1,10 +1,16 @@
 import {audios} from "../../data/audioData";
 import Player from "../../components/musicPlayer";
 import { useState } from "react";
+import {addPropertyControls, ControlType} from "framer";
+import MusicPlayer from "../../components/music_player";
+
 
 export default function Music() {
     const [Songs, setSongs] = useState(audios);
     return (
-        <Player Songs={Songs} />
+        <div>
+            <Player Songs={Songs} />
+            <musicPlayer Songs={Songs} />
+        </div>
     );
 }
