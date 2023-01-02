78ed6012fd881b2a7ffa1659011ca3bcc5b74e29
yeyouchuan <yeyouchuan@onmail.com>
2023-01-02 17:29:55 +0000

    update music_player.tsx

--- pages/experiments/music-player.js
+++ pages/experiments/music-player.js
@@ -1,4 +1,33 @@
+import YouTube, { YouTubeProps } from "react-youtube"
+import { useRef, useState } from "react"
+import { Play, Pause } from "phosphor-react"
+import { motion } from "framer-motion"
+
 export default function Player() {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const player = useRef();
+
+    const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+        player.current = event.target
+    }
+
+    const opts: YouTubeProps["opts"] = {
+        height: "0",
+        width: "0",
+    }
+
+    function togglePlaying() {
+        setIsPlaying((isPlaying) => {
+            if (isPlaying) {
+                player.current.pauseVideo()
+            } else {
+                player.current.playVideo()
+            }
+
+            return !isPlaying
+        })
+    }
+
     return (
         <div className="player mt-6 select-none">
             <div className="mx-auto max-w-[80%] overflow-hidden rounded-xl bg-gradient-to-r from-black via-[#083517] to-[#065321] shadow-md md:max-w-sm">
@@ -7,6 +36,23 @@ export default function Player() {
                         <a className="text-white"></a>
                     </div>
                     <div className="relative md:place-items-center">
+                        <motion.div
+                            onClick={togglePlaying}
+                            whileTap={{ scale: 0.8 }}
+                            className:"w-24 h-24 flex items-center border-[100px] justify-center bg-white cursor-pointer shadow-md"
+                        >
+                            <div className="flex items-center border-0 bg-none appearance-none absolute cursor-pointer">
+                                {!isPlaying && <Play weight="fill" />}
+                                {isPlaying && <Pause weight="fill" />}
+                            </div>
+                        </motion.div>
+                        <div className="hidden absolute">
+                            <YouTube
+                                videoId={props.youtubeId}
+                                opts={opts}
+                                onReady={onPlayerReady}
+                            />
+                        </div>
                         <img className="h-full w-full rounded-md object-cover p-6 md:h-28 md:w-28"
                              src="https://files.catbox.moe/9i0d2g.png" alt="Music cover"/>
                     </div>
@@ -25,6 +71,9 @@ export default function Player() {
                 </div>
             </div>
         </div>
-
     );
+}
+
+MusicPlayer.defaultProps = {
+    youtubeId: "iN3KsbnQZxU",
 }
