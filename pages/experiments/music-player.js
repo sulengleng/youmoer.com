eb62191ef171027a91ba3a6e7b338b6c63a0d3e0
yeyouchuan <yeyouchuan@onmail.com>
2023-01-02 13:36:48 +0000

    update howmpage

--- pages/experiments/music-player.js
+++ pages/experiments/music-player.js
@@ -1,13 +1,11 @@
 import {audios} from "../../data/audioData";
 import Player from "../../components/musicPlayer";
 import { useState } from "react";
-
+import MusicPlayer from "../../components/music_player";
 
 export default function Music() {
     const [Songs, setSongs] = useState(audios);
     return (
-        <Player Songs={Songs}
-        />
+        <Player Songs={Songs}/>
     );
 }
-
