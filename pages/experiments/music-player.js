c55db0f8aaf7687dd568dc8957d38011a5a2b415
yeyouchuan <yeyouchuan@onmail.com>
2023-01-02 19:04:15 +0000

    update music_player.tsx

--- pages/experiments/music-player.js
+++ pages/experiments/music-player.js
@@ -18,9 +18,9 @@ export default function Player() {
                     <div className="relative md:place-items-center">
                         <div className="absolute items-center">
                             {isPlaying ? (
-                                <Pause size={48} className="opacity-50 ml-8 mt-8" color="#f6f5f6" onClick={togglePlay}/>
+                                <Pause size={32} className="opacity-50 ml-8 mt-8" color="#f6f5f6" onClick={togglePlay}/>
                             ) : (
-                                <Play size={48} className="opacity-50 ml-8 mt-8" color="#f6f5f6" onClick={togglePlay}/>
+                                <Play size={32} className="opacity-50 ml-8 mt-8" color="#f6f5f6" onClick={togglePlay}/>
                             )}
                         </div>
                         <img className="h-full w-full rounded-md object-cover p-6 md:h-28 md:w-28"
