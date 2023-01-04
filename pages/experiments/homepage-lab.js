7c30052c2e2b28bffb96f60031053c3049a478ad
yeyouchuan <yeyouchuan@onmail.com>
2023-01-04 15:58:46 +0000

    edit homepage

--- pages/experiments/homepage-lab.js
+++ pages/experiments/homepage-lab.js
@@ -114,7 +114,7 @@ export default function Home() {
                         <div className="md:ml-4 mt-4 max-w-[90%] md:max-w-full mx-auto">
                             <YouTube videoId="xLu_r5FWAIo" opts={opts} onReady={onPlayerReady} className="hidden absolute" />
                             <div className="player select-none">
-                                <div className="drop-shadow-md mx-auto max-w-full overflow-hidden rounded-xl bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] dark:bg-gradient-to-br from-black via-[#083517] to-[#065321] shadow-md md:max-w-sm">
+                                <div className="drop-shadow-md mx-auto max-w-full overflow-hidden rounded-xl bg-gradient-to-tr from-[#fdfbfb] to-[#ebedee] dark:bg-gradient-to-br from-black via-[#083517] to-[#065321] shadow-md md:max-w-sm">
                                     <div className="flex">
                                         <div className="items-center">
                                             <a className="text-white"></a>
