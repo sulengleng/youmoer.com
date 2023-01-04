614bb695ca5f08c63e1046164f76a7a8c396396d
yeyouchuan <yeyouchuan@onmail.com>
2023-01-04 11:22:47 +0000

    edit homepage

--- pages/experiments/homepage-lab.js
+++ pages/experiments/homepage-lab.js
@@ -1,12 +1,25 @@
 import clsx from "clsx";
 import Link from "next/link";
-import {Pause, Play} from "phosphor-react";
+import {ArrowCircleRight, ArrowSquareOut, Pause, Play} from "phosphor-react";
 import { motion } from 'framer-motion'
 import React, {useRef, useState} from "react";
 import YouTube from "react-youtube";
 
 
 export default function Home() {
+    const TIMELINE = [
+        {
+            date: 'Nov 23, 2022',
+            title: '个人网站开始上线',
+            description: '也欢迎你来我之前的数字花园 https://yeyouchuan.top'
+        },
+        {
+            date: 'Sep 28, 2022',
+            title: '开始大学学习',
+            description: '约克大学 Bsc Interactive Media'
+        },
+    ]
+
     const [isPlaying, setIsPlaying] = useState(false);
 
     const audioPlayer = useRef();
@@ -29,6 +42,7 @@ export default function Home() {
             audioPlayer.current.pauseVideo();
         }
     }
+
     return (
     <body className="selection:bg-[#3b818c]">
         <div className="pb-20">
@@ -112,6 +126,39 @@ export default function Home() {
                                 </div>
                             </div>
                         </div>
+
+                        <div
+                            className="border dark:border-gray-800 pt-6 bg-gray-50 md:rounded-lg dark:bg-gray-900"
+                        >
+                            <div className="relative pl-8">
+                                <div className="h-full w-[1px] bg-gray-200 dark:bg-gray-800 left-[10px] top-[5px] absolute"></div>
+                                {TIMELINE?.map((item, index) => (
+                                    <div key={item.title + index} className="mb-10 relative">
+                                        <div className="w-3 h-3 rounded-full absolute left-[-30px] top-[16px]">
+                                            <ArrowCircleRight
+                                                size={18}
+                                                className="bg-white text-gray-400 dark:text-gray-400 dark:bg-gray-900 rounded-full"
+                                            />
+                                        </div>
+                                        <p className="opacity-40 text-xs">{item.date}</p>
+
+                                        <h3 className="leading-tight mb-1 font-semibold text-sm md:text-base inline-block">
+                                            <span className="">{item.title}</span>
+                                            {item?.link ? (
+                                                <Link href={item.link} target="_blank">
+                                                    <span className="inline-block ml-1 opacity-80">
+                                                      <ArrowSquareOut size={12} />
+                                                    </span>
+                                                </Link>
+                                            ) : (
+                                                ""
+                                            )}
+                                        </h3>
+                                        <p className="opacity-60 text-sm">{item.description}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </>
                     </div>
                     <div className="basis-3/7">
                         <div className="container max-w-[90%] mx-auto md:w-112 aspect-square overflow-x-scroll overflow-x-hidden scroll-smooth snap-x flex flex-row items-center bg-white dark:bg-black rounded-lg">
