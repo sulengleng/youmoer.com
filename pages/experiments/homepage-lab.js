9542b0b5b4279e9f10cbe5f9920ffef38ad9369e
yeyouchuan <yeyouchuan@onmail.com>
2023-01-05 18:55:21 +0000

    edit homepage

--- pages/experiments/homepage-lab.js
+++ pages/experiments/homepage-lab.js
@@ -6,7 +6,6 @@ import YouTube from "react-youtube";
 import { StrictMode } from "react";
 import { Notebook } from "../../components/Notebook";
 import { AnimateSharedLayout, AnimatePresence } from "framer-motion";
-import { Route } from "react-router-dom";
 import { clickComponent} from "../../components/click-component";
 import { afterClickComponent} from "../../components/after-click-component";
 
@@ -239,7 +238,11 @@ export default function Home() {
                         <div className="rounded-lg aspect-square md:mr-4 max-w-[90%] md:max-w-full mx-auto my-4">
                             <div className="drop-shadow-md rounded-lg bg-light-map dark:bg-dark-map h-80 max-w-full bg-cover bg-center"></div>
                         </div>
-                        <Route path={["/:id", "/"]} component={Store} />
+                        <div>
+                            <strictMode>
+                                <Store />
+                            </strictMode>
+                        </div>
                     </div>
                 </div>
             </motion.div>
