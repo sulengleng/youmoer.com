38aa52032e8ec65e68181bf59ade0387a85e15b3
yeyouchuan <yeyouchuan@onmail.com>
2023-01-05 15:54:20 +0000

    edit homepage

--- pages/experiments/homepage-lab.js
+++ pages/experiments/homepage-lab.js
@@ -96,8 +96,8 @@ export default function Home() {
 
                         <div className="flex flex-row space-x-4 md:ml-4 mt-4 max-w-[90%] md:max-w-full mx-auto h-48 md:h-56">
                             <div className="basis-1/2">
-                                <div className="rounded-lg h-48 md:h-full drop-shadow-md bg-gradient-to-br from-[#fdfbfb] to-[#ebedee]">
-                                    <a className="text-xs w-[80%] pt-4"><Planet size={20} />夜游船最近有空，请随时通过任何方式和我聊天~</a>
+                                <div className="rounded-lg h-48 md:h-full drop-shadow-md bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] place-items-center">
+                                    <div className=""><Planet size={20} /></div>
                                 </div>
                             </div>
                             <div className="basis-1/2">
