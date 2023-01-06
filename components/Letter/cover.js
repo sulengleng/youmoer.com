71f7ccb6bea361cdb09f472ea209d8d5bcc4e76f
yeyouchuan <yeyouchuan@onmail.com>
2023-01-06 15:58:16 +0000

    edit homepage

--- components/Letter/cover.js
+++ components/Letter/cover.js
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Envelope } from "phosphor-react";
 
-export default function Cover ({ open}) {
+export default function Cover ({ open }) {
   return (
     <motion.div
         className="rounded-lg h-48 md:h-full drop-shadow-md bg-gradient-to-br from-[#fdfbfb] to-[#ebedee]"
