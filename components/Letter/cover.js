c3e0060559f9cee09ac70fd0a142f63fc43bc81d
yeyouchuan <yeyouchuan@onmail.com>
2023-01-06 15:57:32 +0000

    edit homepage

--- components/Letter/cover.js
+++ components/Letter/cover.js
@@ -7,9 +7,8 @@ export default function Cover ({ open}) {
     <motion.div
         className="rounded-lg h-48 md:h-full drop-shadow-md bg-gradient-to-br from-[#fdfbfb] to-[#ebedee]"
         onClick={open}
-        whileHover={{ scale: 1.1 }}
     >
-        <Envelope size={52} />
+        <Envelope size={52} className="ml-28 mt-60" />
     </motion.div>
   );
 }
