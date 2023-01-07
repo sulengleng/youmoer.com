1fc9d1ca5e2e6644218879617e6673587e41e82b
yeyouchuan <yeyouchuan@onmail.com>
2023-01-07 10:31:19 +0000

    edit homepage

--- components/Letter/cover.js
+++ components/Letter/cover.js
@@ -5,10 +5,10 @@ import { Envelope } from "phosphor-react";
 export default function Cover ({ open }) {
   return (
     <motion.div
-        className="rounded-lg h-48 md:h-full drop-shadow-md"
+        className="rounded-lg h-48 md:h-full drop-shadow-md bg-gradient-to-br from-[#fdfbfb] to-[#ebedee]"
         onClick={open}
     >
-        <a className="mt-32 p-8 max-w-sm text-4xl font-black uppercase text-red-800 transform -rotate-6 mix-blend-overlay">Mail</a>
+        <a className="mt-32 p-8 max-w-sm text-4xl font-black uppercase text-red-800 transform -rotate-6">Mail</a>
     </motion.div>
   );
 }
