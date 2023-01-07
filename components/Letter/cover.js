85802f406d13fd7681220b5cb60c22335afea84c
yeyouchuan <yeyouchuan@onmail.com>
2023-01-07 09:46:39 +0000

    edit homepage

--- components/Letter/cover.js
+++ components/Letter/cover.js
@@ -8,7 +8,7 @@ export default function Cover ({ open }) {
         className="rounded-lg h-48 md:h-full drop-shadow-md bg-gradient-to-br from-[#fdfbfb] to-[#ebedee]"
         onClick={open}
     >
-        <div className="drop-shadow-lg border rounded-full bg-gray-600 w-12 h-4 pt-8 justify-center"></div>
+        <div className="drop-shadow-lg border rounded-full bg-gray-600 w-8/12 h-2 pt-8 ml-4 justify-center"></div>
         <a className="p-8 max-w-sm text-2xl font-black uppercase text-red-600 transform -rotate-6 mix-blend-overlay">Mail</a>
     </motion.div>
   );
