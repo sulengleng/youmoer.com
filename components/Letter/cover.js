9d74d93304a3583884f26388863ce1ccb01e0fb7
yeyouchuan <yeyouchuan@onmail.com>
2023-01-07 10:56:08 +0000

    edit homepage

--- components/Letter/cover.js
+++ components/Letter/cover.js
@@ -1,13 +1,29 @@
 import React from "react";
-import { motion } from "framer-motion";
+import {motion, useMotionValue, useTransform} from "framer-motion";
 import { Envelope } from "phosphor-react";
 
 export default function Cover ({ open }) {
+    const x = useMotionValue(200);
+    const y = useMotionValue(200);
+
+    const rotateX = useTransform(y, [0, 400], [45, -45]);
+    const rotateY = useTransform(x, [0, 400], [-45, 45]);
+
+    function handleMouse(event) {
+        const rect = event.currentTarget.getBoundingClientRect();
+
+        x.set(event.clientX - rect.left);
+        y.set(event.clientY - rect.top);
+    }
   return (
     <motion.div
         className="rounded-lg h-48 md:h-full drop-shadow-md bg-gradient-to-br from-[#fdfbfb] to-[#ebedee]"
         onClick={open}
     >
+        <motion.div
+            className="h-12 w-12 bg-white rounded-lg"
+            onMouseMove={handleMouse}
+            style={{ rotateX, rotateY }}></motion.div>
         <a className="mt-32 p-8 max-w-sm text-4xl font-black uppercase text-red-800 transform -rotate-6">Mail</a>
     </motion.div>
   );
--- pages/experiments/homepage-lab.js
+++ pages/experiments/homepage-lab.js
@@ -32,19 +32,6 @@ export default function Home() {
         },
     ]
 
-    const x = useMotionValue(200);
-    const y = useMotionValue(200);
-
-    const rotateX = useTransform(y, [0, 400], [45, -45]);
-    const rotateY = useTransform(x, [0, 400], [-45, 45]);
-
-    function handleMouse(event) {
-        const rect = event.currentTarget.getBoundingClientRect();
-
-        x.set(event.clientX - rect.left);
-        y.set(event.clientY - rect.top);
-    }
-
     const [isPlaying, setIsPlaying] = useState(false);
 
     const audioPlayer = useRef();
@@ -112,16 +99,6 @@ export default function Home() {
                         </div>
                         <motion.div className="basis-1/2">
                             <motion.div className="small-card rounded-lg h-48 md:h-full drop-shadow-md bg-gradient-to-br from-[#fdfbfb] to-[#ebedee]">
-                                <motion.div style={{
-                                    width: 150,
-                                    height: 150,
-                                    borderRadius: 30,
-                                    backgroundColor: "#fff",
-                                    rotateX: rotateX,
-                                    rotateY: rotateY
-                                }}>
-
-                                </motion.div>
                             </motion.div>
                         </motion.div>
                     </motion.div>
