3f3bee46a63cf2066b026c95f7bf6e324e9d699c
yeyouchuan <yeyouchuan@onmail.com>
2023-01-09 02:20:34 +0000

    update homepage-lab.js

--- pages/experiments/homepage-lab.js
+++ pages/experiments/homepage-lab.js
@@ -240,7 +240,7 @@ export default function Home() {
                             className="w-44 h-[100px] flex bg-gradient-to-r from-stone-500 to-stone-700 dark:from-neutral-300 dark:to-stone-400 rounded-lg ml-6 mt-8 drop-shadow-md hover:drop-shadow-xl justify-center items-center"
                         >
                             <div className="-mt-2 text-clip max-h-[100px] overflow-clip text-9xl drop-shadow-md text-gray-400 dark:text-white">₿</div>
-                            <div className="mt-10 ml-12 flex -space-x-3">
+                            <div className="mt-10 ml-10 flex -space-x-3">
                                 <div className="border border-white w-6 h-6 rounded-full bg-gray-800 z-0"></div>
                                 <div className="border border-white w-6 h-6 rounded-full bg-gray-300 z-10"></div>
                             </div>
