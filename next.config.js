15d6b9ac4cdc9c9c4b7f7879cbb37d5628c20e96
yeyouchuan <yeyouchuan@onmail.com>
2023-01-09 22:43:27 +0000

    update homepage-lab.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ module.exports = {
   experimental: {
     urlImports: [
       "https://framer.com/m/",
-      "https://framerusercontent.com/"
+      "https://framerusercontent.com/",
+      "https://fonts.gstatic.com/",
+      "https://ga.jspm.io/",
+      "https://jspm.dev/"
     ]
   }
 }
