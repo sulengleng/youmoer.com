3e979170f25ff87512d186fdb7faceffd131c416
yeyouchuan <yeyouchuan@onmail.com>
2023-01-09 22:33:25 +0000

    update homepage-lab.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   env: {
     instagram: "yeyouchuan",
-    twitter: "yeyouchuan",
+    twitter: "yeyouchuan"
   },
+  experimental: {
+    urlImports: [
+      "https://framer.com/m/",
+      "https://framerusercontent.com/"
+    ]
+  }
 }
