d73170a946b45e9f3ab58414d1607af400752a22
yeyouchuan <yeyouchuan@onmail.com>
2023-01-11 17:41:02 +0000

    update homepage-lab.js

--- components/Letter/cover.js
+++ components/Letter/cover.js
@@ -8,8 +8,6 @@ export default function Cover ({ open }) {
         className="rounded-lg h-48 md:h-full drop-shadow-md bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] flex flex-col"
         onClick={open}
     >
-        <a className="mt-32 p-8 max-w-sm text-4xl font-black uppercase text-red-800 transform rotate-90">Mail</a>
-        <a className="mt-32 p-8 max-w-sm text-4xl font-black uppercase text-red-800 transform rotate-90">Box</a>
     </motion.div>
   );
 }
