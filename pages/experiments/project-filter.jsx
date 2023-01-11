7ceed8b8709d4b830e8178e9a845cefe70b04d99
yeyouchuan <yeyouchuan@onmail.com>
2023-01-11 13:02:41 +0000

    update project-filter.jsx

--- pages/experiments/project-filter.jsx
+++ pages/experiments/project-filter.jsx
@@ -132,7 +132,7 @@ export default function Project() {
     return (
         <div className="w-full max-w-md px-2 py-16 pt-6 sm:px-0">
             <Tab.Group>
-                <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+                <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1 mx-auto max-w-[80%]">
                     {Object.keys(categories).map((category) => (
                         <Tab
                             key={category}
@@ -150,21 +150,6 @@ export default function Project() {
                         </Tab>
                     ))}
                 </Tab.List>
-                <div
-                    className="relative w-full bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-                    <div className="mx-auto max-w-xl">
-                        <nav className="relative z-0 flex justify-around">
-                            <button className="peer/home flex-1">Home</button>
-                            <button className="peer/about flex-1">About</button>
-                            <button className="peer/services flex-1">Services</button>
-                            <button className="peer/blog flex-1">Blog</button>
-                            <button className="peer/contact flex-1">Contact</button>
-
-                            <span
-                                className="absolute left-0 -z-10 h-full w-1/5 origin-center scale-x-110 scale-y-125 rounded-full bg-gray-100 transition-all duration-300 peer-focus/about:left-[20%] peer-focus/services:left-[40%] peer-focus/blog:left-[60%] peer-focus/contact:left-[80%]"></span>
-                        </nav>
-                    </div>
-                </div>
                 <Tab.Panels className="mt-2">
                     {Object.values(categories).map((posts, idx) => (
                         <Tab.Panel
