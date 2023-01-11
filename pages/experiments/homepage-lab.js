845f4ac8d5a1b19a6547293b20b8ae837e280de0
yeyouchuan <yeyouchuan@onmail.com>
2023-01-11 15:50:32 +0000

    update homepage-lab.js

--- pages/experiments/homepage-lab.js
+++ pages/experiments/homepage-lab.js
@@ -202,7 +202,7 @@ export default function Home() {
                 <div className="basis-3/7">
                     <div className="flex flex-col bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] dark:from-black dark:to-[#434343] max-w-[90%] w-[90%] md:w-max md:max-w-fit h-52 mx-auto rounded-lg">
                         <div className="flex">
-                            <div className="bg-camera-left bg-cover rounded-3xl h-28 w-28 ml-10 mr-6 mt-6"></div>
+                            <div className="bg-camera-left bg-cover rounded-3xl h-28 w-28 ml-10 mr-10 mt-6"></div>
                             <div className="flex flex-col justify-items-center">
                                 <div className="flex flex-row mx-auto z-0">
                                     <div className="w-2 h-12 bg-[#c52e1a]"></div>
@@ -213,9 +213,9 @@ export default function Home() {
                                 </div>
                                 <div className="bg-camera-center bg-cover rounded-full h-28 w-28 mx-auto -mt-4 z-10"></div>
                             </div>
-                            <div className="bg-camera-right bg-cover rounded-3xl h-28 w-28 mr-10 ml-6 mt-6"></div>
+                            <div className="bg-camera-right bg-cover rounded-3xl h-28 w-28 mr-10 ml-10 mt-6"></div>
                         </div>
-                        <marquee className="text-lg font-semibold mt-4">滑动照片查看更多~</marquee>
+                        <marquee className="text-lg font-semibold mx-auto w-44">滑动照片查看更多~</marquee>
                     </div>
                     <div className="container max-w-[90%] mx-4 mx-auto md:max-w-fit aspect-square overflow-x-scroll overflow-x-hidden scroll-smooth snap-x flex flex-row items-center bg-white dark:bg-black rounded-lg">
                         <img className="rounded-lg max-h-fit snap-center shadow-md" src="https://s2.loli.net/2022/12/09/SsfYR78lEVtbQ5N.png" alt="music"></img>
