ec9f9b60c332515a21f3f8a9a7f97ab3177e402f
yeyouchuan <yeyouchuan@onmail.com>
2023-01-11 14:50:53 +0000

    update project-filter.jsx

--- next.config.js
+++ next.config.js
@@ -3,15 +3,4 @@ module.exports = {
     instagram: "yeyouchuan",
     twitter: "yeyouchuan"
   },
-  experimental: {
-    urlImports: [
-      "https://framer.com/m/",
-      "https://framerusercontent.com/",
-      "https://fonts.gstatic.com/",
-      "https://ga.jspm.io/",
-      "https://jspm.dev/",
-      "https://s2.loli.net/",
-      "https://thumbs.dreamstime.com/b/"
-    ]
-  }
 }
