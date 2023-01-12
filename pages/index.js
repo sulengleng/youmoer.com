94897857fb8c7e319252baca5e5c67ad0bf81ec3
yeyouchuan <yeyouchuan@onmail.com>
2023-01-12 23:18:24 +0000

    update homepage-lab.js

--- pages/index.js
+++ pages/index.js
@@ -228,7 +228,7 @@ export default function Home() {
                         </div>
                         <div className="relative flex flex-col bg-gradient-to-br mx-4 from-[#fdfbfb] to-[#ebedee] max-w-[90%] w-auto md:w-max md:max-w-fit h-44 mx-auto rounded-lg">
                             <p className="text-lg font-semibold top-4 left-4 absolute w-44">最近看什么？</p>
-                            <img className="rounded-lg mt-10 ml-4" src="https://s2.loli.net/2023/01/07/Lg6OxhpZGSjQqMN.png" alt="cover" />
+                            <img className="rounded-lg h-40 mt-10 ml-4" src="https://s2.loli.net/2023/01/13/aFHc9f2Z7UG8uTd.png" alt="cover" />
                             <a className="text-3xl font-bold mt-14 ml-32 bg-gradient-to-r from-red-300 to-sky-600 bg-clip-text text-transparent">孤独<a className="text-3xl font-bold mt-14 bg-gradient-to-r from-red-500 to-yellow-300 bg-clip-text text-transparent">摇滚</a></a>
                         </div>
                     </div>
