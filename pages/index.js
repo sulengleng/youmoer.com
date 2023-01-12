6d9b2896c73ea63ecbea11ceed53c6050dd4cddb
yeyouchuan <yeyouchuan@onmail.com>
2023-01-12 22:29:00 +0000

    update homepage-lab.js

--- pages/index.js
+++ pages/index.js
@@ -226,8 +226,8 @@ export default function Home() {
                             <img className="rounded-lg max-h-fit snap-center shadow-md" src="https://files.catbox.moe/9i0d2g.png" alt="music"></img>
                             <img className="rounded-lg max-h-fit snap-center shadow-md" src="https://files.catbox.moe/ibe9j9.png" alt="music"></img>
                         </div>
-                        <div className="flex flex-col bg-gradient-to-br mx-4 from-[#fdfbfb] to-[#ebedee] dark:from-black dark:to-[#434343] max-w-[90%] w-auto md:w-max md:max-w-fit h-auto mx-auto rounded-lg">
-
+                        <div className="relative flex flex-col bg-gradient-to-br mx-4 from-[#fdfbfb] to-[#ebedee] dark:from-black dark:to-[#434343] max-w-[90%] w-auto md:w-max md:max-w-fit h-auto mx-auto rounded-lg">
+                            <p className="text-lg font-semibold top-4 left-4 absolute">最近看什么？</p>
                         </div>
                     </div>
                     <div className="basis-1/3 md:basis-2/7 ml-0 md:-ml-4 xl:ml-0">
