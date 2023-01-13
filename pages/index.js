0e91cab3da1a1cba8ef3ac9bf471f88c5ac46b0b
yeyouchuan <yeyouchuan@onmail.com>
2023-01-13 03:21:16 +0000

    update homepage-lab.js

--- pages/index.js
+++ pages/index.js
@@ -260,8 +260,8 @@ export default function Home() {
                                 <a className="ml-2 mt-4 text-lg text-black dark:text-white font-bold text-center leading-4 uppercase font-mono max-w-20">buy me a coffee</a>
                             </motion.div>
                         </motion.div>
-                        <div className="relative bg-[#f2f2f2] dark:bg-[#0d0d0d] ml-0 md:-ml-8 xl:ml-0 md:w-[320px] xl:w-[330] max-w-[90%] md:max-w-full xl:max-w-full mx-auto rounded-lg">
-                            <div className="map absolute invert dark:invert-0 rounded-lg max-w-[90%] md:max-w-[250px] xl:max-w-full mx-auto max-h-64">
+                        <div className="relative bg-[#f2f2f2] dark:bg-[#0d0d0d] ml-0 md:-ml-8 xl:ml-0 md:w-[400px] xl:w-[330] max-w-[90%] md:max-w-full xl:max-w-full mx-auto rounded-lg">
+                            <div className="map absolute top-2 left-2 invert dark:invert-0 rounded-lg max-w-[90%] md:max-w-[250px] xl:max-w-full mx-auto max-h-64">
                                 <StrictMode>
                                     <Notebook />
                                 </StrictMode>
