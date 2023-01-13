e0067e7fbc6b4a962e3c3907ccefcd4b2d7d830e
yeyouchuan <yeyouchuan@onmail.com>
2023-01-13 00:03:19 +0000

    update homepage-lab.js

--- pages/index.js
+++ pages/index.js
@@ -230,7 +230,7 @@ export default function Home() {
                             <img className="rounded-lg h-28 w-auto mt-4 ml-4" src="https://s2.loli.net/2023/01/13/aFHc9f2Z7UG8uTd.png" alt="cover" />
                             <div className="flex flex-col">
                                 <a className="text-lg font-bold mt-2 ml-4 bg-gradient-to-r from-red-300 to-sky-600 bg-clip-text text-transparent">孤独<a className="text-lg font-bold mt-14 bg-gradient-to-r from-red-500 to-yellow-300 bg-clip-text text-transparent">摇滚</a></a>
-                                <p className="text-xs font-medium mt-2 ml-4 mr-6 max-w-2xl">
+                                <p className="text-xs font-medium mt-2 ml-4 mr-6 max-w-lg">
                                     2022年我的年度动画，看完之后很感动。不仅是故事本身的感动，还有对制作组的感动，厨力那么强的作品已经很难见到了！简直我我第二喜欢的作品《派对浪客诸葛孔明》的升级版本，对所有热爱动画的制作人员致以最高的敬意。
                                 </p>
                             </div>
