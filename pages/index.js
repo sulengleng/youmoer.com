0d6e072450874592a6d361e090e0f188140d9210
yeyouchuan <yeyouchuan@onmail.com>
2023-01-13 21:01:38 +0000

    update homepage-lab.js

--- pages/index.js
+++ pages/index.js
@@ -101,7 +101,6 @@ export default function Home() {
                                 </button>
                             </div>
                         </div>
-
                     </motion.div>
 
                 </div>
