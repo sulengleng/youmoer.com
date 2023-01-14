9dd74e574fce9ca6cad4063b9b07f05d52838fc3
yeyouchuan <yeyouchuan@onmail.com>
2023-01-14 19:05:15 +0000

    update homepage-lab.js

--- pages/experiments/reset.js
+++ pages/experiments/reset.js
@@ -69,57 +69,10 @@ export default function Home() {
     return (
         <motion.body className="selection:bg-[#3b818c]">
             <div className="flex flex-row md:grid md:grid-cols-4 gap-4 min-w-screen mt-4">
-                <motion.div className="jike relative md:ml-4 mt-4 max-w-[90%] md:max-w-fit xl:max-w-full mx-auto">
-                    <div className="flex flex-col rounded-lg bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] dark:from-black dark:via-[#756707] dark:to-[#ffdb01] drop-shadow-md">
-                        <div className="flex flex-row">
-                            <img src="https://files.catbox.moe/5scrti.jpg"
-                                 className="mt-4 mx-4 rounded-full h-12 w-12" alt="jike header"/>
-                            <div>
-                                <a className="mt-5 block text-base font-semibold leading-tight text-black dark:text-white">夜游船</a>
-                                <Link className="text-gray-600 dark:text-gray-300 text-xs" href="https://jike.city/yeyouchuan/">@yeyouchuan</Link>
-                            </div>
-                            <img src="https://s2.loli.net/2023/01/03/uCdbKpVMUs2vw17.png"
-                                 className="w-8 h-8 rounded-full absolute top-2.5 right-2.5" alt="jike logo"/>
-                        </div>
-                        <div className="flex flex-row space-x-2 mb-4 mt-4">
-                            <button className="shadow-sm text-sm bg-white dark:bg-grey-800 w-10 h-10 rounded-full ml-5"
-                                    type="button"
-                                    onClick="href='https://m.okjike.com/users/E590A8B2-6A76-455D-BCA7-199A5AE43C7E'">🎉
-                            </button>
-                            <button className="shadow-sm text-sm text-black font-semibold bg-yellow-300 rounded-full w-60 lg:w-60 md:w-48 h-10 pr-4"
-                                    type="button"
-                                    onClick="href='https://m.okjike.com/users/E590A8B2-6A76-455D-BCA7-199A5AE43C7E'">+关注
-                            </button>
-                        </div>
-                    </div>
-                </motion.div>
-                <div className="relative flex flex-row bg-gradient-to-br mt-4 xl:mt-0 mx-4 from-[#fdfbfb] to-[#ebedee] max-w-[90%] w-auto md:w-max md:max-w-fit h-40 md:h-40 xl:h-36 mx-auto rounded-lg">
-                    <img className="rounded-lg h-32 xl:h-28 w-auto mt-4 ml-4" src="https://s2.loli.net/2023/01/13/aFHc9f2Z7UG8uTd.png" alt="cover" />
-                    <div className="flex flex-col">
-                        <a className="text-lg font-bold mt-3 ml-4 bg-gradient-to-r from-red-300 to-sky-600 bg-clip-text text-transparent">孤独<a className="text-lg font-bold bg-gradient-to-r from-red-500 to-yellow-300 bg-clip-text text-transparent">摇滚</a></a>
-                        <p className="text-[10px] md:text-xs font-medium mt-2 ml-4 mr-6 max-w-[220px] xl:max-w-[360px]">
-                            2022年我的年度动画，看完之后很感动。不仅是故事本身的感动，还有对制作组的感动，厨力那么强的作品已经很难见到了！简直我我第二喜欢的作品《派对浪客诸葛孔明》的升级版本，对所有热爱动画的制作人员致以最高的敬意。
-                        </p>
-                    </div>
-                </div>
-                <div className="relative flex flex-row bg-gradient-to-br mt-4 xl:mt-0 mx-4 from-[#fdfbfb] to-[#ebedee] max-w-[90%] w-auto md:w-max md:max-w-fit h-40 md:h-40 xl:h-36 mx-auto rounded-lg">
-                    <img className="rounded-lg h-32 xl:h-28 w-auto mt-4 ml-4" src="https://s2.loli.net/2023/01/13/aFHc9f2Z7UG8uTd.png" alt="cover" />
-                    <div className="flex flex-col">
-                        <a className="text-lg font-bold mt-3 ml-4 bg-gradient-to-r from-red-300 to-sky-600 bg-clip-text text-transparent">孤独<a className="text-lg font-bold bg-gradient-to-r from-red-500 to-yellow-300 bg-clip-text text-transparent">摇滚</a></a>
-                        <p className="text-[10px] md:text-xs font-medium mt-2 ml-4 mr-6 max-w-[220px] xl:max-w-[360px]">
-                            2022年我的年度动画，看完之后很感动。不仅是故事本身的感动，还有对制作组的感动，厨力那么强的作品已经很难见到了！简直我我第二喜欢的作品《派对浪客诸葛孔明》的升级版本，对所有热爱动画的制作人员致以最高的敬意。
-                        </p>
-                    </div>
-                </div>
-                <div className="relative flex flex-row bg-gradient-to-br mt-4 xl:mt-0 mx-4 from-[#fdfbfb] to-[#ebedee] max-w-[90%] w-auto md:w-max md:max-w-fit h-40 md:h-40 xl:h-36 mx-auto rounded-lg">
-                    <img className="rounded-lg h-32 xl:h-28 w-auto mt-4 ml-4" src="https://s2.loli.net/2023/01/13/aFHc9f2Z7UG8uTd.png" alt="cover" />
-                    <div className="flex flex-col">
-                        <a className="text-lg font-bold mt-3 ml-4 bg-gradient-to-r from-red-300 to-sky-600 bg-clip-text text-transparent">孤独<a className="text-lg font-bold bg-gradient-to-r from-red-500 to-yellow-300 bg-clip-text text-transparent">摇滚</a></a>
-                        <p className="text-[10px] md:text-xs font-medium mt-2 ml-4 mr-6 max-w-[220px] xl:max-w-[360px]">
-                            2022年我的年度动画，看完之后很感动。不仅是故事本身的感动，还有对制作组的感动，厨力那么强的作品已经很难见到了！简直我我第二喜欢的作品《派对浪客诸葛孔明》的升级版本，对所有热爱动画的制作人员致以最高的敬意。
-                        </p>
-                    </div>
-                </div>
+                <div className="w-max h-80 bg-gradient-to-r from-slate-300 to-slate-500"></div>
+                <div className="w-max h-64 bg-gradient-to-r from-slate-300 to-slate-500"></div>
+                <div className="w-max h-64 bg-gradient-to-r from-slate-300 to-slate-500"></div>
+                <div className="w-max h-72 bg-gradient-to-r from-slate-300 to-slate-500"></div>
             </div>
         </motion.body>
     );
