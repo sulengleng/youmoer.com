1e95c76f07b47526d53d8f87d13e03dac904a004
yeyouchuan <yeyouchuan@onmail.com>
2023-01-14 20:30:10 +0000

    update homepage-lab.js

--- pages/experiments/reset.js
+++ pages/experiments/reset.js
@@ -69,10 +69,10 @@ export default function Home() {
     return (
         <motion.body className="selection:bg-[#3b818c]">
             <div className="flex flex-row md:grid md:grid-cols-4 gap-4 min-w-screen mt-4 mx-4 ">
-                <div className="w-full h-80 bg-gradient-to-r from-slate-300 to-slate-500"></div>
-                <div className="w-full h-64 bg-gradient-to-r from-slate-300 to-slate-500"></div>
-                <div className="w-full h-64 bg-gradient-to-r from-slate-300 to-slate-500"></div>
-                <div className="w-full h-72 bg-gradient-to-r from-slate-300 to-slate-500"></div>
+                <div className="w-full h-72 bg-gradient-to-r from-neutral-300 to-stone-400"></div>
+                <div className="w-full h-48 bg-gradient-to-r from-neutral-300 to-stone-400"></div>
+                <div className="w-full h-48 bg-gradient-to-r from-neutral-300 to-stone-400"></div>
+                <div className="w-full h-56 bg-gradient-to-r from-neutral-300 to-stone-400"></div>
             </div>
         </motion.body>
     );
