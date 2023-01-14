f2b276be95d1f8c633cef48d6a6c2115ec1f812c
yeyouchuan <yeyouchuan@onmail.com>
2023-01-14 13:56:08 +0000

    update homepage-lab.js

--- pages/experiments/reset.js
+++ pages/experiments/reset.js
@@ -68,19 +68,19 @@ export default function Home() {
 
     return (
         <motion.body className="selection:bg-[#3b818c]">
-            <div className="grid grid-cols-4 gap-4 mt-4 mx-4">
-                <div className="w-full h-80 bg-purple-300 row-span-6">01</div>
-                <div className="w-full h-40 bg-slate-100 row-span-3">02</div>
-                <div className="w-full h-40 bg-red-100 row-span-3">03</div>
-                <div className="w-full h-56 bg-orange-200 row-span-4">04</div>
-                <div className="w-full h-96 bg-orange-200 row-span-6 col-span-2">05</div>
-                <div className="w-full h-24 bg-orange-200 row-span-2 -mt-4">06</div>
-                <div className="w-full h-40 bg-orange-200 row-span-3 -mt-14">07</div>
-                <div className="w-full h-24 bg-orange-200 row-span-2 -mt-8">08</div>
-                <div className="w-full h-56 bg-orange-200 row-span-4 -mt-8">09</div>
-                <div className="w-full h-48 bg-orange-200 row-span-4 -mt-16">10</div>
-                <div className="w-full h-32 bg-orange-200 row-span-3">11</div>
-                <div className="w-full h-32 bg-orange-200 row-span-3">12</div>
+            <div className="grid grid-cols-4 gap-4 mt-4 mx-4 w-screen">
+                <div className="w-full h-80 bg-purple-300">01</div>
+                <div className="w-full h-40 bg-slate-100">02</div>
+                <div className="w-full h-40 bg-red-100">03</div>
+                <div className="w-full h-56 bg-orange-200">04</div>
+                <div className="w-full h-96 bg-orange-200">05</div>
+                <div className="w-full h-24 bg-orange-200">06</div>
+                <div className="w-full h-40 bg-orange-200">07</div>
+                <div className="w-full h-24 bg-orange-200">08</div>
+                <div className="w-full h-56 bg-orange-200">09</div>
+                <div className="w-full h-48 bg-orange-200">10</div>
+                <div className="w-full h-32 bg-orange-200">11</div>
+                <div className="w-full h-32 bg-orange-200">12</div>
             </div>
         </motion.body>
     );
