a77e9d529c780b9d7f80111a86964d47de78cde8
yeyouchuan <yeyouchuan@onmail.com>
2023-01-14 23:41:44 +0000

    update homepage

--- pages/experiments/reset.js
+++ pages/experiments/reset.js
@@ -69,18 +69,17 @@ export default function Home() {
     return (
         <motion.body className="selection:bg-[#3b818c]">
             <div className="flex flex-row md:grid md:grid-cols-4 gap-4 min-w-screen mt-4 mx-4 ">
-                <div className="w-full h-72 bg-gradient-to-r from-neutral-300 to-stone-400 rounded-lg"></div>
-                <div className="w-full h-40 bg-gradient-to-r from-neutral-300 to-stone-400 rounded-lg"></div>
-                <div className="w-full h-40 bg-gradient-to-r from-neutral-300 to-stone-400 rounded-lg"></div>
-                <div className="w-full h-56 bg-gradient-to-r from-neutral-300 to-stone-400 rounded-lg"></div>
-                <div className="w-full h-32 bg-gradient-to-r from-neutral-300 to-stone-400 rounded-lg"></div>
-                <div className="w-full h-128 bg-gradient-to-r from-neutral-300 to-stone-400 rounded-lg col-span-2 -mt-32"></div>
-                <div className="w-full h-80 bg-gradient-to-r from-neutral-300 to-stone-400 rounded-lg -mt-16"></div>
-                <div className="w-full h-40 bg-gradient-to-r from-neutral-300 to-stone-400 rounded-lg -mt-64"></div>
-                <div className="w-full h-40 bg-gradient-to-r from-neutral-300 to-stone-400 rounded-lg"></div>
-                <div className="w-full h-40 bg-gradient-to-r from-neutral-300 to-stone-400 rounded-lg"></div>
-                <div className="w-full h-64 bg-gradient-to-r from-neutral-300 to-stone-400 rounded-lg -mt-32"></div>
-                <div className="w-full h-40 bg-gradient-to-r from-neutral-300 to-stone-400 rounded-lg -mt-52"></div>
+                <div className="w-full h-72 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] rounded-lg"></div>
+                <div className="w-full h-40 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] rounded-lg"></div>
+                <div className="w-full h-40 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] rounded-lg"></div>
+                <div className="w-full h-56 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] rounded-lg"></div>
+                <div className="w-full h-32 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] rounded-lg"></div>
+                <div className="w-full h-128 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] rounded-lg col-span-2 -mt-32"></div>
+                <div className="w-full h-80 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] rounded-lg -mt-16"></div>
+                <div className="w-full h-40 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] rounded-lg -mt-64"></div>
+                <div className="w-full h-40 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] rounded-lg"></div>
+                <div className="w-full h-40 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] rounded-lg col-span-2"></div>
+                <div className="w-full h-64 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] rounded-lg -mt-72"></div>
             </div>
         </motion.body>
     );
