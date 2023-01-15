c7d8071e83b8d4289e8b6cf1259f304f4141b612
yeyouchuan <yeyouchuan@onmail.com>
2023-01-15 18:55:56 +0000

    update homepage

--- pages/experiments/reset.js
+++ pages/experiments/reset.js
@@ -113,17 +113,17 @@ export default function Home() {
                     <div className="border-t mt-4 border-gray-300 dark:border-gray-600"></div>
                     <div className="flex flex-row mt-4 mx-4">
                         <Star size={20} color="#f8d80d" />
-                        <div className="flex flex-col">
+                        <div className="flex flex-col ml-1">
                             <a className="font-semibold">1125</a>
                             <a className="-mt-1 text-sm text-gray-500">Artists</a>
                         </div>
-                        <Disc size={20} color="#6ed49f" />
-                        <div className="flex flex-col">
+                        <Disc size={20} color="#6ed49f" className="ml-3" />
+                        <div className="flex flex-col ml-1">
                             <a className="font-semibold">1942</a>
                             <a className="-mt-1 text-sm text-gray-500">Albums</a>
                         </div>
-                        <MusicNote size={20} color="#548eab" />
-                        <div className="flex flex-col">
+                        <MusicNote size={20} color="#548eab" className="ml-3" />
+                        <div className="flex flex-col ml-1">
                             <a className="font-semibold">2851</a>
                             <a className="-mt-1 text-sm text-gray-500">Tracks</a>
                         </div>
