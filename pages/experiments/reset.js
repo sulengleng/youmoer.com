8392e2e0ed83574896dd1b45f612a406ca56fce2
yeyouchuan <yeyouchuan@onmail.com>
2023-01-15 19:33:14 +0000

    update homepage

--- pages/experiments/reset.js
+++ pages/experiments/reset.js
@@ -190,14 +190,14 @@ export default function Home() {
                                             <span className="absolute inline-flex h-3 w-3 animate-ping rounded-full bg-green-600 opacity-75"></span>
                                             <span className="relative inline-flex h-3 w-3 rounded-full bg-green-500"></span>
                                           </span>
-                                            <div className="w-[87px] xl:w-28 ml-2 mb-1 text-sm md:text-xs xl:text-sm uppercase leading-3 dark:text-white break-normal">now playing</div>
+                                            <div className="w-32 md:w-[87px] xl:w-28 ml-2 mb-1 text-sm md:text-xs xl:text-sm uppercase leading-3 dark:text-white break-normal">now playing</div>
                                         </div>
                                     ) : (
                                         <div className="flex">
                                           <span className="flex h-3 w-3 mt-0 xl:mt-1">
                                             <span className="relative inline-flex h-3 w-3 rounded-full bg-red-500"></span>
                                           </span>
-                                            <div className="w-[86px] xl:w-24 ml-2 mb-1 text-sm md:text-xs xl:text-sm uppercase leading-3 dark:text-white break-normal">not playing</div>
+                                            <div className="w-32 md:w-[86px] xl:w-24 ml-2 mb-1 text-sm md:text-xs xl:text-sm uppercase leading-3 dark:text-white break-normal">not playing</div>
                                         </div>
                                     )}
                                     <div className="flex flex-col">
