c4295133671eae7de70651e4d6a04f67e7d0fc6f
yeyouchuan <yeyouchuan@onmail.com>
2023-01-17 21:35:06 +0000

    update homepage

--- pages/experiments/reset.js
+++ pages/experiments/reset.js
@@ -211,7 +211,7 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="w-full h-128 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] dark:from-black dark:via-[#2e2e2e] dark:to-[#434343] rounded-xl col-span-2 md:-mt-32"></div>
+                <div className="w-full h-80 md:h-128 bg-question bg-repeat rounded-xl col-span-2 md:-mt-32"></div>
                 <div className="w-full h-72 md:h-108 relative flex flex-col bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] dark:from-black dark:via-[#2e2e2e] dark:to-[#434343] rounded-xl md:-mt-16">
                     <div className="absolute mt-6 place-self-center text-lg font-semibold">我喜欢的产品</div>
                     <div className="grid grid-cols-3 md:grid-cols-2 place-self-center ml-0 xl:ml-6 mt-20 md:mt-24 gap-y-6 gap-10">
