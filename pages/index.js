a2fdef5902a1146ec1a2a96203df80a491fc0086
yeyouchuan <yeyouchuan@onmail.com>
2023-01-17 22:30:49 +0000

    update homepage

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import {Disc, MusicNote, Pause, Play, Star} from "phosphor-react";
 import {motion, useMotionValue, useTransform} from 'framer-motion'
 import React, {useRef, useState} from "react";
 import YouTube from "react-youtube";
+import {NextSeo} from "next-seo";
 
 export default function Home() {
     const [isPlaying, setIsPlaying] = useState(false);
@@ -42,6 +43,21 @@ export default function Home() {
 
     return (
         <motion.body className="selection:bg-[#3b818c]">
+            <NextSeo
+                title="你好，我是夜游船！"
+                description="欢迎来到夜游船的个人网站，我是学生、创造者、设计师"
+                openGraph={{
+                    site_name: "你好，我是夜游船！",
+                    title: "你好，我是夜游船！",
+                    description:
+                        "欢迎来到夜游船的个人网站，我是学生、创造者、设计师",
+                }}
+                twitter={{
+                    handle: "@yeyouchuan",
+                    site: "@yeyouchuan",
+                    cardType: "summary_large_image",
+                }}
+            />
             <motion.div
                 initial={{ opacity: 0, y: 20 }}
                 animate={{
