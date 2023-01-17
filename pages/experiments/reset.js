a0e34c153a74f6e666cfdd7961462224f99b41b7
yeyouchuan <yeyouchuan@onmail.com>
2023-01-17 21:00:31 +0000

    update homepage

--- pages/experiments/reset.js
+++ pages/experiments/reset.js
@@ -229,9 +229,9 @@ export default function Home() {
 
                         <p className="mt-4 ml-4 xl:ml-6 text-xs md:text-sm max-w-[280px] xl:max-w-xs font-serif font-semibold">后来他说：“会传染。” 我说：“什么会传染？ 他说：“出海病。”
                             他说：“你望着海。你见有人从海上来，有人从海上行远。你听讲有人再不回头，在一处远得不可思议地方过活。一旦你开始细想那处远得不可思议地方、那种不可思议的远，你就感染出海病。” </p>
-                        <div className="absolute bottom-3 right-3 right-6 flex flex-row">
+                        <div className="absolute bottom-3 right-3 xl:right-6 flex flex-row">
                             <a className="text-xs font-semibold">林棹</a>
-                            <a className="ml-0 md:ml-2 text-xs">潮汐图</a>
+                            <a className="ml-2 text-xs">潮汐图</a>
                         </div>
                     </div>
                     <div className="w-1/3 h-44 rounded-xl bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] dark:from-black dark:via-[#2e2e2e] dark:to-[#434343]">
