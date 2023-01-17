78a523083be0d0866f747dba68f7f1a8bb5d557b
yeyouchuan <yeyouchuan@onmail.com>
2023-01-17 20:05:16 +0000

    update homepage

--- pages/experiments/reset.js
+++ pages/experiments/reset.js
@@ -226,9 +226,10 @@ export default function Home() {
                 <div className="w-full h-40 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] dark:from-black dark:via-[#2e2e2e] dark:to-[#434343] rounded-xl md:-mt-64"></div>
                 <div className="w-full h-44 rounded-xl col-span-2 flex flex-row gap-4">
                     <div className="w-full md:w-2/3 h-44 relative flex flex-col rounded-xl bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] dark:from-black dark:via-[#2e2e2e] dark:to-[#434343]">
-                        <a className="mt-4 ml-4 xl:ml-6 text-lg font-semibold">最近在读 - 潮汐图</a>
-                        <p className="mt-2 ml-4 xl:ml-6 text-xs max-w-[280px] xl:max-w-xs">后来他说：“会传染。” 我说：“什么会传染？ 他说：“出海病。”
+
+                        <p className="mt-4 ml-4 xl:ml-6 text-xs max-w-[280px] xl:max-w-xs">后来他说：“会传染。” 我说：“什么会传染？ 他说：“出海病。”
                             他说：“你望着海。你见有人从海上来，有人从海上行远。你听讲有人再不回头，在一处远得不可思议地方过活。一旦你开始细想那处远得不可思议地方、那种不可思议的远，你就感染出海病。” </p>
+                        <a className="mt-2 ml-4 xl:ml-6 text-am font-semibold">——《潮汐图》</a>
                     </div>
                     <div className="w-1/3 h-44 rounded-xl bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] dark:from-black dark:via-[#2e2e2e] dark:to-[#434343]">
                         <img className="mx-auto my-auto mt-6 md:mt-0 mt-4 h-auto md:h-[85%] w-[95%] md:w-auto rounded-lg" src="https://s2.loli.net/2023/01/17/7lzrw29VK4SIokB.png" alt="book cover"></img>
