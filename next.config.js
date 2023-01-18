297b492672955f11ed3f5e1c1470bc407c0ae60b
yeyouchuan <yeyouchuan@onmail.com>
2023-01-18 18:52:27 +0000

    update homepage

--- next.config.js
+++ next.config.js
@@ -3,13 +3,4 @@ module.exports = {
     instagram: "yeyouchuan",
     twitter: "yeyouchuan"
   },
-  experimental: {
-    urlImports: [
-      "https://framer.com/m/",
-      "https://framerusercontent.com/",
-      "https://ga.jspm.io/",
-      "https://jspm.dev/",
-      "https://s2.loli.net/"
-    ],
-  },
 }
