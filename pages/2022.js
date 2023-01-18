4953368b33d28492172a268fddb15f017e292fef
yeyouchuan <yeyouchuan@onmail.com>
2023-01-18 11:05:29 +0000

    update homepage

--- pages/2022.js
+++ pages/2022.js
@@ -57,7 +57,7 @@ export default function Home() {
       />
 
       <article className="w-full px-5 pb-10 overflow-y-auto max-w-[620px] mx-auto">
-        <h2 className="mb-6 mt-8 text-3xl font-black md:text-4xl dark:text-white">仿生人夜游船(2022批次)使用手册</h2>
+        <h2 className="mb-6 mt-12 text-3xl font-black md:text-4xl dark:text-white">仿生人夜游船(2022批次)使用手册</h2>
 
           <ContentWrapper width="700px" className="ml-auto mb-4 sm:text-left">
               <h2 className="font-bold text-lg flex items-center">
