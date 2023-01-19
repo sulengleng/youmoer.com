40b1f6b7a16377ccee3aea59538b468cb40e2eab
yeyouchuan <yeyouchuan@onmail.com>
2023-01-19 00:32:43 +0000

    update homepage

--- pages/index.js
+++ pages/index.js
@@ -225,8 +225,8 @@ export default function Home() {
                         </div>
                     </div>
                     <div className="w-full h-40 relative flex flex-col bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] dark:from-gray-800 dark:via-gray-900 dark:to-black rounded-xl md:-mt-64">
-                        <p className="mt-4 ml-4 font-semibold">Powered by</p>
-                        <p className="mt-4 ml-3 flex flex-col text-lg font-medium">
+                        <p className="mt-4 ml-6 text-lg font-semibold">Powered by</p>
+                        <p className="mt-3 ml-5 flex flex-col text-lg font-medium">
                             <span className="inline-flex items-baseline">
                               <img src="https://s2.loli.net/2023/01/19/ON7aHI2CVqwFGrk.png" alt="" className="h-8 w-9 self-center"/>
                               <a href="https://www.github.com/">Github</a>
