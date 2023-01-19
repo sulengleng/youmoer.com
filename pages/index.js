364523a4a06b12d832de95c521fae711ffe7c9ec
yeyouchuan <yeyouchuan@onmail.com>
2023-01-19 00:51:50 +0000

    update homepage

--- pages/index.js
+++ pages/index.js
@@ -233,7 +233,8 @@ export default function Home() {
                               <a href="https://tailwindcss.com/">Tailwind CSS</a>
                             </span>
                             <span className="inline-flex items-baseline">
-                              <img src="https://s2.loli.net/2023/01/19/ON7aHI2CVqwFGrk.png" alt="" className="h-8 w-9 self-center"/>
+                              <img src="https://s2.loli.net/2023/01/19/ON7aHI2CVqwFGrk.png" alt=""
+                                   className="mt-1 -ml-1 h-8 w-9 self-center"/>
                               <a href="https://www.github.com/">Github</a>
                             </span>
                             <span className="inline-flex items-baseline">
