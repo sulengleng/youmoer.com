7f36ec71f08ff0d185eef9ea05039ebdefdfee50
yeyouchuan <yeyouchuan@onmail.com>
2023-01-19 19:43:03 +0000

    update homepage

--- pages/2022.js
+++ pages/2022.js
@@ -145,7 +145,7 @@ export default function Home() {
                   <SpotifyLogo size={20} className="mr-2 opacity-40" />
                   What I see and hear
               </h2>
-              <div className="mx-8 grid h-72 grid-flow-col grid-rows-3 gap-2 lg:mx-72">
+              <div className="grid h-72 grid-flow-col grid-rows-3 gap-2 lg:mx-72">
                   <div className="row-span-3 rounded-lg bg-gradient-to-br from-slate-100 to-slate-300">
                       <p className="ml-4 mt-4 text-lg font-semibold">01 万能青年旅店-万能青年旅店</p>
                       <ul className="mt-2 space-y-1 text-sm">
