6dc0b68da60d3da556df081bebae3456a54e5129
yeyouchuan <yeyouchuan@onmail.com>
2023-01-20 19:51:31 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -251,7 +251,7 @@ export default function Home() {
                   What I see and hear
               </h2>
               <div className="py-0 md:py-5 mt-5 md:mt-0">
-                  <details className="group max-w-xl mx-auto">
+                  <details className="group max-w-[610px] mx-auto">
                       <summary className="flex cursor-pointer list-none md:justify-between font-medium">
                           <span> 年度最喜欢的十个剧集</span>
                           <span className="transition group-open:rotate-180 invisible md:visible">
