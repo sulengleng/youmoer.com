55a6adecdeb7bb3f3dc2feefdfa7ad9ec1dc5b95
yeyouchuan <yeyouchuan@onmail.com>
2023-01-20 15:20:51 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -209,7 +209,7 @@ export default function Home() {
               </h2>
               <div className="py-5">
                   <details className="group">
-                      <summary className="flex cursor-pointer list-none items-center justify-between font-medium">
+                      <summary className="flex cursor-pointer list-none justify-between font-medium">
                           <span> 年度最喜欢的十个剧集</span>
                           <span className="transition group-open:rotate-180">
                             <CaretDown size={24} />
