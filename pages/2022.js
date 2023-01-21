a4b014fddb78bb1a5ef5022a62825aff9cc82372
yeyouchuan <yeyouchuan@onmail.com>
2023-01-21 14:43:01 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -258,7 +258,7 @@ export default function Home() {
               >
                   <div className="flex gap-6">
                       <img className="rounded-xl rotate-3 mt-4 h-20 w-20" src="https://s2.loli.net/2023/01/19/JVYUhcwtvEA5mx3.png"  alt=""/>
-                      <p className="text-lg font-semibold">Readwise Reader</p>
+                      <p className="text-xl font-semibold mt-8">Readwise Reader</p>
                   </div>
                   <div className="my-4 prose prose-gray dark:prose-zinc dark:text-gray-200 mx-auto lg:prose-lg">
                       <p>
