66704ab3cf7da79db2091cf6336bccbdeebab12d
yeyouchuan <yeyouchuan@onmail.com>
2023-01-21 23:32:36 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -381,13 +381,13 @@ export default function Home() {
                             <CaretDown size={24} />
                           </span>
                       </summary>
-                      <div className="mx-4 grid grid-cols-3 lg:grid-cols-5 my-6 md:my-4 md:justify-center gap-5 md:gap-10 !max-w-[260px] md:!max-w-[580px] h-0 md:h-fit hidden md:visible">
+                      <div className="mx-4 grid grid-cols-3 lg:grid-cols-5 my-6 md:my-4 md:justify-center gap-5 md:gap-10 !max-w-[260px] md:!max-w-[580px] h-0 md:h-fit">
                           {IMAGES?.map((item) => (
                               <div
                                   key={item.src}
                                   className={clsx(
                                       item?.className,
-                                      "hidden md:visible relative border p-[4px] max-w-[110px] md:max-w-fit shadow-[0_0_32px_rgba(0,0,0,0.1)] rounded-[12px] dark:border-gray-700"
+                                      "relative border p-[4px] max-w-[110px] md:max-w-fit shadow-[0_0_32px_rgba(0,0,0,0.1)] rounded-[12px] dark:border-gray-700"
                                   )}
                               >
                                   <img
