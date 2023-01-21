4c61885b00c4b0e3ae0f6bc03bb643ca0609e580
yeyouchuan <yeyouchuan@onmail.com>
2023-01-21 14:35:06 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -252,20 +252,24 @@ export default function Home() {
                       个人系统今年与去年相比确实有不少不同，下面我来分别讲讲：
                   </p>
               </div>
-              <div className="mt-4 prose prose-gray dark:prose-zinc dark:text-gray-200 mx-auto lg:prose-lg">
-                  <div className="mt-4">
-                      <img className="float-right rounded-xl h-20 w-20" src="https://s2.loli.net/2023/01/19/JVYUhcwtvEA5mx3.png"  alt=""/>
+              <ContentWrapper
+                  width="700px"
+                  className="border dark:border-gray-800 pt-6 bg-gray-50 md:rounded-lg dark:bg-gray-900"
+              >
+                  <div>
+                      <img className="rounded-xl rotate-[2dge] mt-4 h-20 w-20" src="https://s2.loli.net/2023/01/19/JVYUhcwtvEA5mx3.png"  alt=""/>
+                  </div>
+                  <div className="mt-4 prose prose-gray dark:prose-zinc dark:text-gray-200 mx-auto lg:prose-lg">
                       <p>
                           今年阅读量的增加其实挺靠 Readwise Reader 的，今年在 Reader 上阅读了超过了 2000 篇文章，创建了 1768 个高亮和笔记。
                           创建高亮最多的一篇文章是<a href="https://foresightnews.pro/article/detail/6630">详解 Y Combinator 发家史与投资组合演变：如何创造 6000 亿美元总价值？</a>,
                           一共43个高亮~
                       </p>
-                      <br/>
                       <p>
                           不谈那些最优秀的阅读软件的特点，我觉得最打动我的几点分别是：阅读光标、高亮兼容性、文章分类。
                       </p>
                   </div>
-              </div>
+              </ContentWrapper>
           </ContentWrapper>
 
           <br/>
