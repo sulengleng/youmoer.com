9c28c62f88278dfd43d80e2c17ed0d34381c9920
yeyouchuan <yeyouchuan@onmail.com>
2023-01-21 22:08:05 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -377,11 +377,11 @@ export default function Home() {
                           <span className="my-4 ml-4 text-lg font-semibold bg-gradient-to-r from-orange-500 to-indigo-500 bg-clip-text text-transparent">
                               年度最喜欢的十个剧集
                           </span>
-                          <span className="my-4 transition group-open:rotate-180 invisible md:visible">
+                          <span className="my-4 mr-4 transition group-open:rotate-180 invisible md:visible">
                             <CaretDown size={24} />
                           </span>
                       </summary>
-                      <div className="mx-4 grid grid-cols-5 lg:grid-cols-5 my-6 md:my-4 justify-center gap-5 md:gap-10 !max-w-[580px]">
+                      <div className="mx-4 grid grid-cols-5 lg:grid-cols-5 my-6 md:my-4 justify-center gap-5 md:gap-10 !max-w-[200px] md:!max-w-[580px]">
                           {IMAGES?.map((item) => (
                               <div
                                   key={item.src}
