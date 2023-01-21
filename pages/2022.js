72dfe8f1a2cb3738cbbe1b67928e86263b7e9536
yeyouchuan <yeyouchuan@onmail.com>
2023-01-21 22:05:49 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -381,7 +381,7 @@ export default function Home() {
                             <CaretDown size={24} />
                           </span>
                       </summary>
-                      <div className="mx-4 grid grid-cols-5 lg:grid-cols-5 my-6 md:my-4 justify-center gap-5 md:gap-10 !max-w-[500px]">
+                      <div className="mx-4 grid grid-cols-5 lg:grid-cols-5 my-6 md:my-4 justify-center gap-5 md:gap-10 !max-w-[580px]">
                           {IMAGES?.map((item) => (
                               <div
                                   key={item.src}
