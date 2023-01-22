a06635a4ebd6a8617a2309108f8669458985c71d
yeyouchuan <yeyouchuan@onmail.com>
2023-01-22 16:38:30 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -481,7 +481,7 @@ export default function Home() {
               <div className="py-0 md:py-5 mt-5 md:mt-0">
                   <details className="group max-w-[625px] mx-auto">
                       <summary className="flex cursor-pointer list-none md:justify-between font-medium">
-                          <span className="my-4 text-lg font-semibold bg-gradient-to-r from-orange-500 to-indigo-500 bg-clip-text text-transparent">
+                          <span className="my-4 text-lg font-semibold bg-gradient-to-r from-red-600 to-yellow-400 bg-clip-text text-transparent">
                               年度最喜欢的十张专辑
                           </span>
                           <span className="my-4 mr-4 transition group-open:rotate-180 collapse md:visible">
