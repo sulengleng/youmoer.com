53e4b8151ade38038f1e5546aac89ad2ea73b9c9
yeyouchuan <yeyouchuan@onmail.com>
2023-01-23 11:00:42 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -606,10 +606,6 @@ export default function Home() {
                               </div>
                               <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                                   <a href="#" target="_self"
-                                     className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">
-                                      Deactivate
-                                  </a>
-                                  <a href="#" target="_self"
                                      className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                                       Cancel
                                   </a>
