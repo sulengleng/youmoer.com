cf0ac5808c0ed97455e3d816df2718b0bf73442a
yeyouchuan <yeyouchuan@onmail.com>
2023-01-23 19:34:28 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -636,8 +636,8 @@ export default function Home() {
                                             </span>
                                           </a>
                                           <div
-                                          className="relative mb-16 sm:mb-0 scale-90 transform overflow-hidden rounded-lg bg-white dark:bg-gray-900/60 text-left opacity-0 shadow-xl transition-all duration-300 group-target:scale-100 group-target:opacity-100 sm:my-8 sm:w-full sm:max-w-lg">
-                                              <div className="bg-white dark:bg-gray-900/60 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                          className="relative mb-16 sm:mb-0 scale-90 transform overflow-hidden rounded-lg bg-white dark:bg-gray-900/80 text-left opacity-0 shadow-xl transition-all duration-300 group-target:scale-100 group-target:opacity-100 sm:my-8 sm:w-full sm:max-w-lg">
+                                              <div className="bg-white dark:bg-gray-900/80 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                                                   <div className="sm:flex sm:items-start">
                                                       <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                                                         <h3 className="text-lg font-medium leading-6 text-gray-900"
@@ -650,9 +650,9 @@ export default function Home() {
                                                       </div>
                                                   </div>
                                               </div>
-                                              <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                              <div className="bg-gray-50 dark:bg-gray-900/80 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                                                   <a href="#photos-year" target="_self"
-                                                  className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                                                  className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-900/80 px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                                                   关闭
                                                   </a>
                                               </div>
