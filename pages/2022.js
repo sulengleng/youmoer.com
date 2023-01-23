804529e7f37c9acfa58692f9ffce743dc92de62b
yeyouchuan <yeyouchuan@onmail.com>
2023-01-23 10:36:34 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -575,6 +575,53 @@ export default function Home() {
                   <Camera size={20} className="mr-2 opacity-40" />
                   Photos of the Year
               </h2>
+              <div className="relative">
+                  <!-- // In real project replace the href with just "#modal", "about:src" is only for tailwind play -->
+                  <a href="#modal" target="_self"
+                     className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Open
+                      Modal</a>
+              </div>
+              <div
+                  className="group invisible relative z-10 opacity-0 transition-all duration-300 target:visible target:opacity-100"
+                  aria-labelledby="modal-title" role="dialog" aria-modal="true" id="modal">
+                  <div className="fixed inset-0 z-10 overflow-y-auto">
+                      <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                          <!-- // Backdrop + Close on clicking outside -->
+                          <!-- // In real project replace the href with just "#", "about:src" is only for tailwind play -->
+                          <a href="#" target="_self"
+                             className="fixed inset-0 block cursor-default bg-black/50 bg-opacity-75 transition-opacity"><span
+                              className="sr-only">Close Modal</span></a>
+
+                          <div
+                              className="relative scale-90 transform overflow-hidden rounded-lg bg-white text-left opacity-0 shadow-xl transition-all duration-300 group-target:scale-100 group-target:opacity-100 sm:my-8 sm:w-full sm:max-w-lg">
+                              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                  <div className="sm:flex sm:items-start">
+                                      <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                          <h3 className="text-lg font-medium leading-6 text-gray-900"
+                                              id="modal-title">Deactivate account</h3>
+                                          <div className="mt-2">
+                                              <p className="text-sm text-gray-500">Are you sure you want to deactivate
+                                                  your account? All of your data will be permanently removed. This
+                                                  action cannot be undone.</p>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                              <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                  <!-- // In real project replace the href with just "#", "about:src" is only for tailwind play -->
+                                  <a href="#" target="_self"
+                                     className="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">
+                                      Deactivate
+                                  </a>
+                                  <a href="#" target="_self"
+                                     className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                                      Cancel
+                                  </a>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
           </ContentWrapper>
           <ContentWrapper>
               <h2 className="font-bold text-lg flex items-center">
