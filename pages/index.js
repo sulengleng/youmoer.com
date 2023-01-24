eea27e999b54b97239a028b0052da1ed7ca9d8c0
yeyouchuan <yeyouchuan@onmail.com>
2023-01-24 11:04:23 +0000

    update 2022.js

--- pages/index.js
+++ pages/index.js
@@ -104,7 +104,7 @@ export default function Home() {
                     </div>
                     <div className="w-full h-40 bg-gradient-to-br from-[#fdfbfb] to-[#ebedee] dark:from-gray-800 dark:via-gray-900 dark:to-black rounded-xl flex flex-col relative">
                         <div className="flex flex-col">
-                            <p className="mt-4 ml-5 md:ml-4 text-2xl font-semibold">10431</p>
+                            <p className="mt-4 ml-5 md:ml-4 text-2xl font-semibold">10750</p>
                             <p className="-mt-1 ml-6 md:ml-5 text-sm text-gray-500">Scrobbles</p>
                         </div>
                         <img src="https://s2.loli.net/2023/01/16/cAOCi9IwasjXSHB.png"
@@ -113,17 +113,17 @@ export default function Home() {
                         <div className="flex flex-row mt-4 mx-5 md:mx-4">
                             <Star size={20} color="#f8d80d" />
                             <div className="flex flex-col ml-1 -mt-1 md:mt-0">
-                                <p className="font-semibold">1125</p>
+                                <p className="font-semibold">1126</p>
                                 <p className="-mt-1 text-sm text-gray-500">Artists</p>
                             </div>
                             <Disc size={20} color="#6ed49f" className="ml-8 md:ml-3" />
                             <div className="flex flex-col ml-1 -mt-1 md:mt-0">
-                                <p className="font-semibold">1948</p>
+                                <p className="font-semibold">1959</p>
                                 <p className="-mt-1 text-sm text-gray-500">Albums</p>
                             </div>
                             <MusicNote size={20} color="#548eab" className="ml-8 md:ml-3" />
                             <div className="flex flex-col ml-1 -mt-1 md:mt-0">
-                                <p className="font-semibold">2862</p>
+                                <p className="font-semibold">2878</p>
                                 <p className="-mt-1 text-sm text-gray-500">Tracks</p>
                             </div>
                         </div>
@@ -136,9 +136,9 @@ export default function Home() {
                         <img src="https://s2.loli.net/2023/01/16/4PhMLmkjT9SQUti.png"
                              className="absolute top-3 right-3 h-9 w-9 rounded-full border-[#34b6de]" alt="xyzfm logo"/>
                         <div className="flex flex-row">
-                            <p className="mt-10 ml-4 text-4xl font-semibold">244</p>
+                            <p className="mt-10 ml-4 text-4xl font-semibold">254</p>
                             <p className="mt-14 ml-1 text-sm text-gray-500">小时</p>
-                            <p className="mt-10 ml-1 text-4xl font-semibold">22</p>
+                            <p className="mt-10 ml-1 text-4xl font-semibold">41</p>
                             <p className="mt-14 ml-1 text-sm text-gray-500">分钟</p>
                         </div>
                     </div>
