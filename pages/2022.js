fe4330b030c4c071b58aec695f7d4f253fe46f84
yeyouchuan <yeyouchuan@onmail.com>
2023-01-25 09:33:05 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -682,18 +682,13 @@ export default function Home() {
           </ContentWrapper>
           <div className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-black z-0">
               <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0">
-                  <div
-                      className="absolute inset-auto right-1/2 h-56 w-[30rem] bg-gradient-conic from-pink-500 via-black to-transparent text-white [--conic-position:from_70deg_at_center_top]"></div>
-                  <div
-                      className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-black to-pink-500 text-white [--conic-position:from_290deg_at_center_top]"></div>
+                  <div className="absolute inset-auto right-1/2 h-56 w-[30rem] bg-gradient-conic from-pink-500 via-black to-transparent text-white [--conic-position:from_70deg_at_center_top]"></div>
+                  <div className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-black to-pink-500 text-white [--conic-position:from_290deg_at_center_top]"></div>
                   <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-black blur-2xl"></div>
                   <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
-                  <div
-                      className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-pink-500 opacity-50 blur-3xl"></div>
-                  <div
-                      className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-pink-400 blur-2xl"></div>
-                  <div
-                      className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-pink-400 blur-sm"></div>
+                  <div className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-pink-500 opacity-50 blur-3xl"></div>
+                  <div className="absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-pink-400 blur-2xl"></div>
+                  <div className="absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-pink-400 blur-sm"></div>
 
                   <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-black"></div>
               </div>
