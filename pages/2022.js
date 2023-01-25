c443d73702e4c90c67ffc9cf04b2d8b5cabc1644
yeyouchuan <yeyouchuan@onmail.com>
2023-01-25 11:18:23 +0000

    update 2022.js

--- pages/2022.js
+++ pages/2022.js
@@ -695,7 +695,6 @@ export default function Home() {
               <div className="absolute top-12 z-50 text-2xl font-bold text-white">夜游船渡口</div>
               <div className="absolute top-24 z-50 text-white">夜游船的 newsletter，欢迎订阅和我交流</div>
               <span className="absolute top-44 z-50 h-9 w-16 cursor-pointer rounded-xl border bg-white bg-opacity-50 bg-clip-padding pt-1 text-center font-medium text-gray-200 backdrop-blur"
-                    ref="subscribe"
               >
                   订阅
               </span>
