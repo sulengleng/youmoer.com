d906a1d63e0abb639f0f4bea74f5dc59a1e6902d
yeyouchuan <yeyouchuan@onmail.com>
2023-02-01 12:07:07 +0000

    homepage.js

--- pages/index.js
+++ pages/index.js
@@ -148,6 +148,15 @@ export default function Home() {
                             onMouseMove={handleMouse}
                             style={{ height:168 }}
                         >
+                            <div className="">
+                                <div
+                                    className="absolute inset-auto h-24 w-24 -translate-x-8 translate-y-16 scale-50 rounded-full bg-gradient-to-br from-red-300 via-pink-400 to-purple-200 sm:translate-x-20 sm:translate-y-28 sm:scale-100"></div>
+                            </div>
+
+                            <div className="">
+                                <div
+                                    className="absolute inset-auto h-24 w-24 -translate-x-56 -translate-y-16 scale-50 rounded-full bg-gradient-to-tr from-red-300 via-pink-300 to-orange-100 sm:-translate-x-80 sm:-translate-y-28 sm:scale-100"></div>
+                            </div>
                             <motion.div
                                 style={{rotateX: rotateX, rotateY: rotateY}}
                                 className="mask pointer-events-none relative z-10 w-40 xl:w-52 h-[100px] xl:h-[130px] mt-6 md:mt-8 ml-0 md:ml-0 select-none rounded-xl bg-[#262f70]/20 p-4 backdrop-blur-2xl after:absolute after:inset-0 after:rounded-xl after:bg-gradient-to-br after:from-white/80 after:via-white/10 after:to-white/80 after:p-px"
