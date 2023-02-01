39c57a8f9f6b1069a6866008ef077fe830d211b9
yeyouchuan <yeyouchuan@onmail.com>
2023-02-01 23:42:13 +0000

    homepage.js

--- pages/index.js
+++ pages/index.js
@@ -148,11 +148,11 @@ export default function Home() {
                             onMouseMove={handleMouse}
                             style={{ height:168 }}
                         >
-                            <div className="absolute top-2 -left-4 h-14 w-14 scale-50 rounded-full bg-gradient-to-br from-red-300 via-pink-400 to-purple-200 sm:scale-100"></div>
-                            <div className="absolute top-28 -right-4 h-14 w-14 scale-50 rounded-full bg-gradient-to-tr from-red-300 via-pink-300 to-orange-100 sm:scale-100"></div>
+                            <div className="absolute top-2 left-6 h-14 w-14 scale-50 rounded-full bg-gradient-to-br from-red-300 via-pink-400 to-purple-200 sm:scale-100"></div>
+                            <div className="absolute top-28 right-6 h-14 w-14 scale-50 rounded-full bg-gradient-to-tr from-red-300 via-pink-300 to-orange-100 sm:scale-100"></div>
                             <motion.div
                                 style={{rotateX: rotateX, rotateY: rotateY}}
-                                className="mask pointer-events-none relative z-10 w-40 xl:w-52 h-[100px] xl:h-[130px] mt-6 md:mt-8 ml-0 md:ml-0 select-none rounded-xl bg-[#262f70]/20 p-4 backdrop-blur-2xl after:absolute after:inset-0 after:rounded-xl after:bg-gradient-to-br after:from-white/80 after:via-white/10 after:to-white/80 after:p-px"
+                                className="mask pointer-events-none relative z-10 w-40 xl:w-52 h-[100px] xl:h-[130px] mt-6 md:mt-8 ml-0 md:ml-0 xl:ml-10 select-none rounded-xl bg-[#262f70]/20 p-4 backdrop-blur-2xl after:absolute after:inset-0 after:rounded-xl after:bg-gradient-to-br after:from-white/80 after:via-white/10 after:to-white/80 after:p-px"
                             >
                                 <div>
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 opacity-75 mt-6"
