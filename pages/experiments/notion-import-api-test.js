4ace3e6be98bc533292f4218dd06c90191c26b73
yeyouchuan <yeyouchuan@onmail.com>
2023-02-05 10:32:06 +0000

    music

--- pages/experiments/notion-import-api-test.js
+++ pages/experiments/notion-import-api-test.js
@@ -1,11 +1,7 @@
-import { List } from "phosphor-react"
 const { Client } = require("@notionhq/client")
 import Link from "next/link"
-import classnames from "classnames";
-import { ArrowIcon } from "lib/icons";
-import {data} from "autoprefixer";
 
-export default function NotionImportApiTest ({ reading }) {
+export default function NotionImportApiTest () {
   return (
     <div>
       <h1>Notion API Test</h1>
