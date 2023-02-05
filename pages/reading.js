92cab59a22266b18605f832c0dd1595334513eea
yeyouchuan <yeyouchuan@onmail.com>
2023-02-05 17:42:57 +0000

    music

--- pages/reading.js
+++ pages/reading.js
@@ -10,22 +10,12 @@ export default function Reading ({ reading }) {
         <body>
             <div className="max-w-[90%] md:max-w-[75%] mx-auto mt-8">
                 <h1 className="text-xl font-bold">阅读文章高亮一览</h1>
-                <table className="table-auto md:table-fixed mt-4">
-                    <thead>
-                    <tr>
-                        <th className="text-left">文章</th>
-                        <th className="text-left">作者</th>
-                    </tr>
-                    </thead>
-                    {reading.map((read) => (
-                        <tbody className="">
-                            <tr>
-                                <td className="">{read.title}</td>
-                                <td className="">{read.author}</td>
-                            </tr>
-                        </tbody>
-                    ))}
-                </table>
+                {reading.map((read) => (
+                <div className="mt-1 flex h-12 w-full justify-between rounded-lg bg-gray-200">
+                    <p className="max-w-1/2 ml-4 place-self-center">{read.title}</p>
+                    <p className="mr-4 place-self-center">{read.author}</p>
+                </div>
+                ))}
             </div>
         </body>
     )
