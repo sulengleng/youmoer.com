c1bf6ecda564ae314f30e62f41521e0e65e3e364
yeyouchuan <yeyouchuan@onmail.com>
2023-02-05 17:45:40 +0000

    music

--- pages/reading.js
+++ pages/reading.js
@@ -8,8 +8,8 @@ import {data} from "autoprefixer";
 export default function Reading ({ reading }) {
     return (
         <body>
-            <div className="max-w-[90%] md:max-w-[75%] mx-auto mt-8">
-                <h1 className="text-xl font-bold">阅读文章高亮一览</h1>
+            <div className="max-w-[90%] md:max-w-[75%] mx-auto mt-4">
+                <h1 className="text-2xl font-bold my-6">阅读文章高亮一览</h1>
                 {reading.map((read) => (
                 <div className="mt-1 flex h-12 w-full justify-between rounded-lg bg-gray-200 dark:dark:bg-gray-800/70">
                     <p className="max-w-1/2 ml-4 place-self-center">{read.title}</p>
