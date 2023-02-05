639f20368394e10b7e4c953dd85d50d25e3d852a
yeyouchuan <yeyouchuan@onmail.com>
2023-02-05 18:01:23 +0000

    music

--- pages/reading.js
+++ pages/reading.js
@@ -1,15 +1,20 @@
-import { List } from "phosphor-react"
+import {RoughNotation} from "react-rough-notation";
 const { Client } = require("@notionhq/client")
-import Link from "next/link"
-import classnames from "classnames";
-import { ArrowIcon } from "lib/icons";
-import {data} from "autoprefixer";
+
 
 export default function Reading ({ reading }) {
     return (
         <body>
             <div className="max-w-[90%] md:max-w-[75%] mx-auto mt-4">
-                <h1 className="text-2xl font-bold my-6">阅读文章高亮一览</h1>
+                <RoughNotation
+                    type="highlight"
+                    show={true}
+                    color="#ffc72e"
+                    animationDuration={1500}
+                    className="text-2xl font-bold mx-auto my-6"
+                >
+                    阅读文章高亮一览
+                </RoughNotation>
                 {reading.map((read) => (
                 <div className="mt-1 flex h-12 w-full justify-between rounded-lg bg-gray-200 dark:bg-gray-800">
                     <p className="max-w-1/2 ml-4 place-self-center text-gray-900 dark:text-gray-50">{read.title}</p>
