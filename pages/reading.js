5709accda6408ef7eb91693e8152175eb7ffe21a
yeyouchuan <yeyouchuan@onmail.com>
2023-02-13 14:10:53 +0000

    homepage

--- pages/reading.js
+++ pages/reading.js
@@ -6,7 +6,7 @@ const { Client } = require("@notionhq/client")
 export default function Reading ({ reading }) {
     return (
         <body>
-            <div className="max-w-[90%] max-w-[80%] mx-auto mt-4">
+            <div className="max-w-[90%] max-w-[80%] mx-auto mt-4 flex flex-col">
                 <div className="mx-auto my-6 md:my-8">
                     <RoughNotation
                         type="highlight"
