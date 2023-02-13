288a7c77da2667a6ff7fa42765bba2eb5af4b584
yeyouchuan <yeyouchuan@onmail.com>
2023-02-13 19:06:25 +0000

    reading.js

--- pages/reading.js
+++ pages/reading.js
@@ -21,7 +21,20 @@ export default function Reading ({ reading, latest }) {
                 <div className="py-2 mx-auto justify-center text-center text-sm text-gray-500 bg-repeat bg-[url('https://s2.loli.net/2022/12/07/3MBKFxyaXtjNrGh.png')] dark:bg-[url('https://s2.loli.net/2022/12/07/9cv1jB2W6ZOVFfk.png')] dark:text-white">
                     以下记录了我所有在阅读时 highlight 过的文章，数据通过 Notion Database API 获取。阅读文章的记录由 Readwise Reade 提供。
                 </div>
-                <notionLatest className="w-full h-64 rounded-lg bg-#38e8be"></notionLatest>
+                <div className="w-full h-64 rounded-lg bg-#38e8be">
+                    {latest.map((last) => (
+                        <div className="flex flex-col w-full h-24">
+                            <a
+                                className="px-1 md:px-10 font-bold text-gray-900 dark:text-gray-50 hover:text-[#ffc72e] hover:underline underline-offset-4 decoration-wavy"
+                                href={last.link}
+                                target="_blank"
+                            >
+                                {last.title}
+                            </a>
+                            <p className="py-4 px-1 md:px-10 text-gray-900 dark:text-gray-50">{last.author}</p>
+                        </div>
+                    ))}
+                </div>
                 <div className="my-8 md:divide-x divide-dotted divide-gray-300 flex flex-col md:grid md:grid-cols-3 gap-4 min-w-screen">
                     {reading.map((read) => (
                         <div className="flex flex-col w-full h-24">
@@ -80,5 +93,50 @@ export async function getStaticProps() {
             reading,
         },
         revalidate: 60,
+    }
+}
+
+export async function getRecentHighlight() {
+    const notion = new Client({
+        auth: "secret_ig3DqLAGXUCS9bSiZZXvIDalhynwwQkmLjUS42udYLo",
+    })
+
+    const data = await notion.databases.query({
+        database_id: "08204d7869154037bd52912de7a6f10d",
+        filter: {
+            and: [
+                {
+                    property: "Category",
+                    select: {
+                        equals: "Articles",
+                    },
+                },
+                {
+                    property: "Last Highlighted",
+                    date: {
+                        this_week: {},
+                    },
+                }
+            ],
+        },
+        sorts: [
+            {
+                property: "Last Highlighted",
+                direction: "descending",
+            },
+        ],
+    })
+
+    const latest = data.results.map(last => ({
+        id: last.id,
+        title: last.properties.Title.title[0].plain_text,
+        author: last.properties.Author.rich_text[0].plain_text,
+        link: last.properties.Link.url,
+    }))
+
+    return {
+        props: {
+            latest,
+        },
     }
 }
