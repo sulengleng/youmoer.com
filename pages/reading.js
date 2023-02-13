d394b049e8135b13e0f25f0a12a0387bb6424c31
yeyouchuan <yeyouchuan@onmail.com>
2023-02-13 17:25:16 +0000

    homepage

--- pages/reading.js
+++ pages/reading.js
@@ -21,7 +21,20 @@ export default function Reading ({ reading, latest }) {
                 <div className="py-2 mx-auto justify-center text-center text-sm text-gray-500 bg-repeat bg-[url('https://s2.loli.net/2022/12/07/3MBKFxyaXtjNrGh.png')] dark:bg-[url('https://s2.loli.net/2022/12/07/9cv1jB2W6ZOVFfk.png')] dark:text-white">
                     以下记录了我所有在阅读时 highlight 过的文章，数据通过 Notion Database API 获取。阅读文章的记录由 Readwise Reade 提供。
                 </div>
-
+                <div className="w-full h-64 flex flex-col">
+                    {latest.map((last) => (
+                        <div className="flex flex-col w-full h-24">
+                            <a
+                                className="px-1 md:px-10 font-bold text-gray-900 dark:text-gray-50 hover:text-[#ffc72e] hover:underline underline-offset-4 decoration-wavy"
+                                href={last.link}
+                                target="_blank"
+                            >
+                                {last.title}
+                            </a>
+                            <p className="py-4 px-1 md:px-10 text-gray-900 dark:text-gray-50">{last.author}</p>
+                        </div>
+                    ))}
+                </div>
                 <div className="my-8 md:divide-x divide-dotted divide-gray-300 flex flex-col md:grid md:grid-cols-3 gap-4 min-w-screen">
                     {reading.map((read) => (
                         <div className="flex flex-col w-full h-24">
