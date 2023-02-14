d29c182b5b090c19af946b2289ffdf9418348bf2
yeyouchuan <yeyouchuan@onmail.com>
2023-02-14 01:13:57 +0000

    reading.js

--- pages/reading.js
+++ pages/reading.js
@@ -20,7 +20,7 @@ export default function Reading ({ reading, latest, lighted }) {
                     以下记录了我所有在阅读时 highlight 过的文章，数据通过 Notion Database API 获取。阅读文章的记录由 Readwise Reade 提供。
                 </div>
                 <div className="flex md:flex-row flex-col gap-x-4">
-                    <div className="mt-8 w-full h-auto rounded-lg bg-[#38e8be] relative">
+                    <div className="mt-8 w-full h-auto rounded-lg bg-[#38e8be] relative selection:bg-[#5200ff]">
                         <p className="pt-6 pl-4 md:pl-6 text-2xl text-[#5200ff] font-extrabold z-10">本周阅读</p>
                         {latest.map((last) => (
                             <div className="flex flex-col w-full h-auto max-w-[200px] md:max-w-[250px] z-10">
@@ -37,12 +37,12 @@ export default function Reading ({ reading, latest, lighted }) {
                         <div className="w-full h-6 z-0"></div>
                         <h1 className="absolute z-0 bottom-4 right-4 text-6xl font-black uppercase text-white transform -rotate-90 mix-blend-overlay">Week</h1>
                     </div>
-                    <div className="mt-8 w-full h-auto rounded-lg bg-[#e8b1d2] relative">
-                        <p className="pt-6 pl-4 md:pl-6 text-2xl text-[#2d52b7] font-extrabold z-10">最多高亮</p>
+                    <div className="mt-8 w-full h-auto rounded-lg bg-[#e8b1d2] relative selection:bg-[#3d70fa]">
+                        <p className="pt-6 pl-4 md:pl-6 text-2xl text-[#3d70fa] font-extrabold z-10">最多高亮</p>
                         {lighted.map((light) => (
                             <div className="flex flex-col w-full h-auto max-w-[200px] md:max-w-[250px] z-10">
                                 <a
-                                    className="z-20 pl-4 pt-4 md:pl-6 text-lg font-bold text-gray-900 hover:decoration-[#5200ff] hover:underline underline-offset-4 decoration-wavy"
+                                    className="z-20 pl-4 pt-4 md:pl-6 text-lg font-bold text-gray-900 hover:decoration-[#3d70fa] hover:underline underline-offset-4 decoration-wavy"
                                     href={light.link}
                                     target="_blank"
                                 >
@@ -50,8 +50,8 @@ export default function Reading ({ reading, latest, lighted }) {
                                 </a>
                                 <div className="z-10 flex flex-row">
                                     <p className="z-20 pl-4 px-1 md:pl-6 text-gray-900">{light.author}</p>
-                                    <diV className="z-20 h-6 w-6 rounded-full self-center bg-[#e398c6]">
-                                        <p className="z-30 text-xs font-bold text-[#5200ff] text-center pt-1">{light.highlights}</p>
+                                    <diV className="z-20 h-6 w-6 rounded-full self-center bg-[#e398c6] ml-2">
+                                        <p className="z-30 text-xs font-bold text-[#3d70fa] text-center pt-1">{light.highlights}</p>
                                     </diV>
                                 </div>
                             </div>
@@ -59,7 +59,7 @@ export default function Reading ({ reading, latest, lighted }) {
                         <div className="w-full h-6 z-10"></div>
                         <h1 className="absolute z-0 bottom-4 right-4 text-6xl font-black uppercase text-white transform -rotate-90 mix-blend-overlay">Highlight</h1>
                     </div>
-                    <div className="mt-8 w-full h-auto rounded-lg bg-[#4c02e8] relative">
+                    <div className="mt-8 w-full h-auto rounded-lg bg-[#4c02e8] relative selection:bg-[#3cffd0]">
                         <p className="pt-6 pl-4 md:pl-6 text-2xl text-[#3cffd0] font-extrabold z-10">最爱阅读</p>
                         {latest.map((last) => (
                             <div className="flex flex-col w-full h-auto max-w-[200px] md:max-w-[250px] z-10">
@@ -78,6 +78,7 @@ export default function Reading ({ reading, latest, lighted }) {
                     </div>
                 </div>
                 <div className="my-8 flex flex-col md:grid md:grid-cols-3 gap-4 min-w-screen bg-[#aa9649] dark:bg-black rounded-lg">
+                    <div className="flex flex-col">
                     <p className="pt-6 pl-4 md:pl-6 text-2xl text-[#ffc72e] font-extrabold">全部高亮</p>
                     {reading.map((read) => (
                         <div className="flex flex-col w-full h-24">
@@ -91,6 +92,7 @@ export default function Reading ({ reading, latest, lighted }) {
                             <p className="pl-4 px-1 md:pl-6 text-gray-900 dark:text-gray-50">{read.author}</p>
                         </div>
                     ))}
+                    </div>
                 </div>
             </div>
         </body>
