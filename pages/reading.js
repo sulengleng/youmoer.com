e6e4a76b3f7d95521c48978cc960537468d056bc
yeyouchuan <yeyouchuan@onmail.com>
2023-02-14 12:49:13 +0000

    reading.js

--- pages/reading.js
+++ pages/reading.js
@@ -89,7 +89,7 @@ export default function Reading ({ reading, latest, lighted }) {
                         </div>
                     ))}
                 </div>
-                <div className="py-2 mx-auto justify-center text-center text-sm text-gray-500 bg-repeat bg-[url('https://s2.loli.net/2022/12/07/3MBKFxyaXtjNrGh.png')] dark:bg-[url('https://s2.loli.net/2022/12/07/9cv1jB2W6ZOVFfk.png')] dark:text-white">
+                <div className="py-2 mb-8 mx-auto justify-center text-center text-sm text-gray-500 bg-repeat bg-[url('https://s2.loli.net/2022/12/07/3MBKFxyaXtjNrGh.png')] dark:bg-[url('https://s2.loli.net/2022/12/07/9cv1jB2W6ZOVFfk.png')] dark:text-white">
                     以下记录了我所有在阅读时 highlight 过的文章，数据通过 Notion Database API 获取。阅读文章的记录由 Readwise Reade 提供。
                 </div>
             </div>
