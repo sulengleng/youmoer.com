5afd80f8bbf5a8d4f52bc7454279449ce2ab62a2
yeyouchuan <yeyouchuan@onmail.com>
2023-02-17 00:07:40 +0000

    reading.js

--- pages/reading.js
+++ pages/reading.js
@@ -71,7 +71,7 @@ export default function Reading ({ reading, latest, lighted }) {
                             </div>
                         ))}
                         <div className="w-full h-6 z-0"></div>
-                        <h1 className="absolute z-0 bottom-56 -right-14 text-8xl font-black font-thunder uppercase text-white transform -rotate-90 mix-blend-overlay">Favorite</h1>
+                        <h1 className="absolute z-0 bottom-24 -right-16 text-8xl font-black font-thunder uppercase text-white transform -rotate-90 mix-blend-overlay">Favorite</h1>
                     </div>
                 </div>
                 <div className="my-8 flex flex-col md:grid md:grid-cols-3 gap-4 min-w-screen border-dashed border-2 border-[#ffc72e] bg-gray-50 dark:bg-black rounded-lg">
