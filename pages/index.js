daa2020bc698f1e3713a51e3945139260e750060
yeyouchuan <yeyouchuan@gamil.com>
2023-06-27 00:53:23 +0800

    更新博客

--- pages/index.js
+++ pages/index.js
@@ -31,6 +31,18 @@ export default function Home() {
                 }}
                 className="page"
             >
+                <div className="flex flex-col w-[90%] md:w-[600px] lg:w-[850px] xl:w-[1000px] mx-auto mt-8">
+                    <p className="text-2xl">你好，我是<a className="bg-gradient-to-r from-teal-200 to-teal-500 bg-clip-text text-transparent font-bold">夜游船</a>！</p>
+                    <p className="text-base mt-4">新版的主页正在修缮中，请下次再来看看~</p>
+                </div>
+                <div className="flex flex-col md:grid md:grid-cols-3 gap-6 min-w-screen w-[90%] md:w-[600px] lg:w-[850px] xl:w-[1000px] mx-auto mt-8 ">
+                    <div className="w-full h-[330px] bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#16171a] to-[#16171a] border border-gray-100 drop-shadow rounded-3xl row-span-2"></div>
+                    <div className="w-full h-44 bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#16171a] to-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-3xl row-span-1"></div>
+                    <div className="w-full h-[460px] bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#16171a] to-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-3xl row-span-2"></div>
+                    <div className="w-full h-32 bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#16171a] to-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-3xl row-span-1 -mt-32"></div>
+                    <div className="w-full h-[320px] bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#16171a] to-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-3xl row-span-1 col-span-2 -mt-32"></div>
+                    <div className="w-full h-48 bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#16171a] to-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-3xl row-span-1"></div>
+                </div>
             </motion.div>
         </motion.body>
     );
