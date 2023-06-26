1708969d9938159a9441c6236065670215a1db37
yeyouchuan <yeyouchuan@gamil.com>
2023-06-26 14:13:22 +0800

    update

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { EmailTemplate } from "../../components/email-template.tsx";
+import EmailTemplate from "../../components/email-template.jsx";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
