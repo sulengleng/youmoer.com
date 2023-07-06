c702633f5984e0bff95f06c36add8ded2638e4f2
yeyouchuan <yeyouchuan@gamil.com>
2023-07-07 00:17:15 +0800

    更新博客

---
+++ pages/api/send.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+        const data = await resend.emails.send({
+            from: "hello@yeyouchuan.me",
+            to: "yeyouchuan@gmail.com",
+            subject: "Hello world",
+            html: "<strong>It works!</strong>",
+        });
+
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(400).json(error);
+    }
+};
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import {Disc, MusicNote, Pause, Play, Star} from "phosphor-react";
-import {motion, useMotionValue, useTransform} from 'framer-motion'
+import {motion, useMotionValue, useTransform, AnimatePresence} from 'framer-motion'
 import React, {useRef, useState} from "react";
 import YouTube from "react-youtube";
 import {NextSeo} from "next-seo";
 
 export default function Home() {
+
     return (
         <motion.body className="select-none">
             <NextSeo
@@ -31,19 +32,64 @@ export default function Home() {
                 }}
                 className="page"
             >
-                <div className="flex flex-col w-[90%] md:w-[600px] lg:w-[850px] xl:w-[1000px] mx-auto mt-8">
-                    <p className="text-2xl">你好，我是<a className="bg-gradient-to-r from-teal-200 to-teal-500 bg-clip-text text-transparent font-bold">夜游船</a>！</p>
-                    <p className="text-base mt-4">新版的主页正在修缮中，请下次再来看看~</p>
+                <div className="flex flex-col w-[85%] md:w-[600px] lg:w-[850px] xl:w-[1000px] mx-auto mt-8">
+                    <GradualSpacing/>
+                    <BlurIn/>
                 </div>
                 <div className="flex flex-col md:grid md:grid-cols-3 gap-6 min-w-screen w-[90%] md:w-[600px] lg:w-[850px] xl:w-[1000px] mx-auto mt-8 ">
-                    <div className="w-full h-[330px] bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#16171a] to-[#16171a] border border-gray-100 drop-shadow rounded-3xl row-span-2"></div>
-                    <div className="w-full h-44 bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#16171a] to-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-3xl row-span-1"></div>
-                    <div className="w-full h-[460px] bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#16171a] to-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-3xl row-span-2"></div>
-                    <div className="w-full h-32 bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#16171a] to-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-3xl row-span-1 -mt-32"></div>
-                    <div className="w-full h-[320px] bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#16171a] to-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-3xl row-span-1 col-span-2 -mt-32"></div>
-                    <div className="w-full h-48 bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#16171a] to-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-3xl row-span-1"></div>
+                    <div className="w-full h-[330px] bg-white dark:bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-[#367d62] via-[#538c72] to-[#78c9a3] border dark:border-none border-gray-100 drop-shadow rounded-2xl row-span-2"></div>
+                    <div className="w-full h-44 bg-white dark:bg-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-2xl row-span-1"></div>
+                    <div className="w-full h-[460px] bg-white dark:bg-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-2xl row-span-2"></div>
+                    <div className="w-full h-32 bg-white dark:bg-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-2xl row-span-1 -mt-32"></div>
+                    <div className="w-full h-[320px] bg-white dark:bg-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-2xl row-span-1 col-span-2 -mt-32"></div>
+                    <div className="w-full h-48 bg-white dark:bg-[#16171a] border dark:border-none border-gray-100 drop-shadow rounded-2xl row-span-1"></div>
                 </div>
             </motion.div>
         </motion.body>
+    );
+}
+
+export function GradualSpacing() {
+    const text = "你好，我是夜游船！";
+    const gradual = {
+        hidden: { opacity: 0, x: -20 },
+        visible: { opacity: 1, x: 0 },
+    };
+    return (
+        <div className="flex space-x-0.5">
+            <AnimatePresence>
+                {text.split("").map((char, i) => (
+                    <motion.h1
+                        key={i}
+                        initial="hidden"
+                        animate="visible"
+                        exit="hidden"
+                        variants={gradual}
+                        transition={{ duration: 0.5, delay: i * 0.1 }}
+                        className="font-display text-3xl font-semibold tracking-[-0.02em] drop-shadow-sm md:text-3xl md:leading-[5rem]"
+                    >
+                        {char === " " ? <span>&nbsp;</span> : char}
+                    </motion.h1>
+                ))}
+            </AnimatePresence>
+        </div>
+    );
+}
+
+export function BlurIn() {
+    const variants1 = {
+        hidden: { filter: "blur(10px)", opacity: 0 },
+        visible: { filter: "blur(0px)", opacity: 1 },
+    };
+    return (
+        <motion.p
+            initial="hidden"
+            animate="visible"
+            transition={{ duration: 1 }}
+            variants={variants1}
+            className="font-display text-lg font-normal tracking-[-0.02em] drop-shadow-sm mt-4 md:mt-0"
+        >
+            我的个人网站主页目前正在施工中，敬请期待！可以随时看看其他页面~
+        </motion.p>
     );
 }
