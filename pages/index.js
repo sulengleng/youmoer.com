381145775ab2daba934ff846de1436da2b8d5ec9
yeyouchuan <yeyouchuan@gamil.com>
2023-07-07 18:51:55 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -93,7 +93,7 @@ export function BlurIn() {
         >
             <div className="p-2 rounded-t-xl rounded-bl-md rounded-br-xl bg-gray-100 dark:bg-gray-800 w-fit">我的个人网站还在装修中，你可以随时看看其他页面~</div>
             <div className="p-2 rounded-xl bg-gray-100 dark:bg-gray-800 w-fit">我目前在中国苏州，也是英国本科学生。</div>
-            <diV className="flex items-center space-x-2">
+            <diV className="flex items-center space-x-2 pb-4">
                 <img 
                     src="https://s2.loli.net/2023/07/07/Fgcf35yDCpM9vxL.png" 
                     alt="头像" 
@@ -102,7 +102,7 @@ export function BlurIn() {
                 </img>
                 <p className="text-[15px] text-gray-500 dark:text-gray-300">3m ago</p>
             </diV>
-            <div className="p-2 rounded-xl bg-gray-100 dark:bg-gray-800 w-fit">我目前在中国苏州，也是英国本科学生。</div>
+            <div className="p-2 rounded-xl bg-gray-100 dark:bg-gray-800 w-fit">我平时会看漫画和动漫，也对独立摇滚感兴趣。</div>
         </motion.div>
     );
 }
