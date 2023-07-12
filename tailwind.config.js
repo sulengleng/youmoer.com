a4fee19dc9e5353eba7b587c21ead53a282ae6e3
yeyouchuan <yeyouchuan@gamil.com>
2023-07-13 00:30:25 +0800

    updata

--- tailwind.config.js
+++ tailwind.config.js
@@ -44,6 +44,7 @@ module.exports = {
         "white-star":'url(https://s2.loli.net/2023/07/08/j3qRMCnYUfJGum7.png)',
         "black-star":'url(https://s2.loli.net/2023/07/08/zNB3VLQaDM7omcp.png)',
         "book-store":'url(https://s2.loli.net/2023/07/10/5qA2rgZ6WXT3Czc.png)',
+        "dark-board":'url(https://s2.loli.net/2023/07/13/yAVbuhS7CO2oewx.png)',
       },
       gridTemplateRows: {
         '[auto,auto,1fr]': 'auto auto 1fr',
