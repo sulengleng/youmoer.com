e9811187dc9ced08d3bca19a0ec16cc0582e80e7
yeyouchuan <yeyouchuan@gamil.com>
2023-08-21 18:19:35 +0800

    update

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -40,35 +40,3 @@ export function getAllPosts(fields = []) {
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
   return posts;
 }
-
-import RSS from 'rss'
-
-const getRSS = async () => {
-  const feed = new RSS({
-    title: "夜游船渡口",
-    description: '随便写东西的地方',
-    site_url: 'https://yeyouchuan.me/',
-    feed_url: 'https://yeyouchuan.me/feed.xml',
-    language: 'cn',
-    pubDate: new Date(),
-    copyright: `All rights reserved ${new Date().getFullYear()}, 夜游船`
-  });
-
-  const data = await fetchData() // 获取文章数据才能填充 RSS feed
-  // 假设 data 是一个类型为文章的数组：
-  data.forEach((post) => {
-    feed.item({
-      title: post.title, // 文章名
-      guid: post.id, // 文章 ID
-      url: `https://yeyouchuan.me/blog/${post.slug}`, // 文章的链接
-      description: post.description, // 文章的介绍，如果有的话
-      date: post.date// 文章的发布时间
-    })
-  })
-
-  return new Response(feed.xml(), {
-    headers: {
-      'content-type': 'application/xml'
-    }
-  })
-}
